70094ed77d5303ea5f53d21f5e7cdec698b1555f
Vlad Mocanu <vladmoca90@gmail.com>
2024-01-20 00:19:37 +0000

    Reversed everything back

--- miniprojects/src/app/page.tsx
+++ miniprojects/src/app/page.tsx
@@ -21,7 +21,7 @@ export default function FoodList() {
         setFoods(data.body);
     }, [foodsUrl]);
 
-    // A function that calculates the total price
+    //A function that calculates the total price
     const calculateTotalPrice = useCallback(() => {
         let sum = 0;
 
@@ -32,7 +32,7 @@ export default function FoodList() {
         return sum;
     }, [foods]);
 
-    // A function that calculates the average price of all items combined
+    //A function that calculates the average price of all items combined
     const calculateAveragePrice = useCallback(() => {
         let average = 0;
 
@@ -43,7 +43,7 @@ export default function FoodList() {
         return average;
     }, [foods]);
 
-    // A function that returns the highest price of an item
+    //A function that returns the highest price of an item
     const getMostExpensiveItem = useCallback(() => {
         let expensive = 0;
 
