c155c4077f09b067feabfb3673a4029e839dd678
Vlad Mocanu <vladmoca90@gmail.com>
2024-01-28 20:45:10 +0000

    Tested

--- miniprojects/src/app/wine-details/page.tsx
+++ miniprojects/src/app/wine-details/page.tsx
@@ -1,10 +1,28 @@
+"use client";
+import Image from "next/image";
+import "./../styles/wines.css";
 
 export default function WineDetails({ searchParams }: {
     searchParams: {
         "wineName": string,
     }
 }) {
-    
+
     let wineNameUrl = "http://localhost:3000/api/get-wine" + searchParams.wineName;
+
+    return (
+        <section className="box wine-details">
+            <div className="products-container">
+                <div className="product">
+                    <div className="product-description__top">
+                        <p className="product-title"></p>
+                    </div>
+                    <div className="product-description__bottom">
+                        <Image alt="" className="product-img" width={200} height={100} src="" />
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
 
 }
