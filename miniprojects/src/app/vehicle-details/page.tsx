01f3f7d3b38c91460647236f3ca84d564eb7f419
Vlad Mocanu <vladmoca90@gmail.com>
2024-01-29 19:48:43 +0000

    Styled the car details page

--- miniprojects/src/app/vehicle-details/page.tsx
+++ miniprojects/src/app/vehicle-details/page.tsx
@@ -1,15 +1,14 @@
+/* eslint-disable @next/next/no-img-element */
 "use client";
 import { useCallback, useEffect, useState } from "react";
+import Link from "next/link";
 import { Car } from "../../../lib/carsShowroom/Car";
-
-// BE CAREFUL TO KEEP THE SAME NAMES EVERYWHERE!
 
 export default function VehicleDetails({ searchParams }: {
     searchParams: {
         "carModel": string
     }
 }) {
-
     let modelsUrl = "http://localhost:3000/api/get-car-model?carModel=" + searchParams.carModel;
 
     const [carDetails, setCarDetails] = useState<Car>([] as any);
@@ -39,12 +38,23 @@ export default function VehicleDetails({ searchParams }: {
     }, [getCarDetails]);
 
     return (
-        <div>
-            <h3>Vehicle details of {searchParams.carModel}</h3>
-            <p>{carDetails.img}</p>
-            <p>{carDetails.make}</p>
-            <p>{searchParams.carModel}</p>
-            <p>{carDetails.price}</p>
+        <div id="showroom">
+            <div className="car-container">
+                <div className="car-header">
+                    <h3 className="car-title">{carDetails.make} <span>{searchParams.carModel}</span></h3>
+                    <p className="car-price">&pound;{carDetails.price}
+                        <span className="car-monthly-price">from &pound;{(carDetails.price / 12).toFixed(0)}/monthly</span>
+                    </p>
+                </div>
+                <div className="car-img-container">
+                    <img alt={carDetails.make} className="car-img" src={carDetails.img} />
+                </div>
+                <div className="showroom-buttons">
+                    <Link href="#">Enquiry</Link>
+                    <Link href="#">Share</Link>
+                    <Link href="#">Brochure</Link>
+                </div>
+            </div>
         </div>
     );
 }
