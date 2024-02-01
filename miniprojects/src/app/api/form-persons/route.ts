395c1b049daf508181f98c145890d29f20d617b3
Vlad Mocanu <vladmoca90@gmail.com>
2024-02-01 23:53:30 +0000

    Fixed it to POST

--- miniprojects/src/app/api/form-persons/route.ts
+++ miniprojects/src/app/api/form-persons/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 import { allPersons } from "../../../../lib/formNames/allPersons";
 
-export async function GET(request: NextRequest) {
+export async function POST(request: NextRequest) {
     return NextResponse.json(
         {
             body: allPersons,
