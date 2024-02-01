a9702471196476afbc195915d754a1c3b22cf1cd
Vlad Mocanu <vladmoca90@gmail.com>
2024-02-01 23:31:06 +0000

    Added a complete list of persons. 4 in total.

--- miniprojects/lib/formNames/allPersons.ts
+++ miniprojects/lib/formNames/allPersons.ts
@@ -10,11 +10,27 @@ export const allPersons: Person[] = [
         password: "7ZEXFY236kL",
     },
     {
-        name: "Alexandru Mocanu",
-        age: 33,
-        nationality: "Romanian",
-        profession: "Backend Developer",
-        weight: 77,
+        name: "Peter Jackson",
+        age: 62,
+        nationality: "New Zealander",
+        profession: "Film producer",
+        weight: 88,
         password: "A756MNuwegh",
-    }
+    },
+    {
+        name: "Mia Zattinni",
+        age: 40,
+        nationality: "Italian",
+        profession: "Bank manager",
+        weight: 69,
+        password: "Gaansbaai746384",
+    },
+    {
+        name: "Darius Namazie",
+        age: 26,
+        nationality: "Iranian",
+        profession: "Architect",
+        weight: 75,
+        password: "045YhNFDRghul",
+    },
 ];
