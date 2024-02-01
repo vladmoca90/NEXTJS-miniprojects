16bfb244b3b04d894330cca0c7cada7a7ba069be
Vlad Mocanu <vladmoca90@gmail.com>
2024-02-01 21:50:47 +0000

    Renamed all as form-submission

---
+++ miniprojects/src/app/page.tsx
@@ -0,0 +1,38 @@
+"use client";
+import "./../app/styles/form-submission.css";
+
+export default function FormSubmission() {
+    return (
+        <form className="w-full max-w-sm form-container">
+            <div className="md:flex md:items-center mb-6">
+                <div className="md:w-1/3">
+                    <label className="block text-gray-500 font-bold md:text-center mb-1 md:mb-0 pr-4">Full Name</label>
+                </div>
+                <div className="md:w-2/3">
+                    <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-full-name" type="text" placeholder="Jane Doe" />
+                </div>
+            </div>
+            <div className="md:flex md:items-center mb-6">
+                <div className="md:w-1/3">
+                    <label className="block text-gray-500 font-bold md:text-center mb-1 md:mb-0 pr-4">Password</label>
+                </div>
+                <div className="md:w-2/3">
+                    <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-password" type="password" placeholder="******************" />
+                </div>
+            </div>
+            <div className="md:flex md:items-center mb-6">
+                <div className="md:w-1/3"></div>
+                <label className="md:w-2/3 block text-gray-500 font-bold form-checkbox">
+                    <input className="mr-2 leading-tight" type="checkbox" />
+                    <span className="text-sm">Confirm terms and conditions!</span>
+                </label>
+            </div>
+            <div className="md:flex md:items-center">
+                <div className="md:w-1/3"></div>
+                <div className="md:w-2/3">
+                    <button className="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="button">Submit</button>
+                </div>
+            </div>
+        </form>
+    );
+}
