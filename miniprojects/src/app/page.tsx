ce64295f0e50fc1621e5a76ce41f137eb6cca7f6
Vlad Mocanu <vladmoca90@gmail.com>
2024-02-03 20:13:18 +0000

    Formatted the file

--- miniprojects/src/app/page.tsx
+++ miniprojects/src/app/page.tsx
@@ -36,42 +36,42 @@ export default function FormPerson() {
     }, [nameText, passwordText]);
 
     return (
-            <form className="w-full max-w-sm form-container">
-                <div className="md:flex md:items-center mb-6">
-                    <div className="md:w-1/3">
-                        <label className="block text-gray-500 font-bold md:text-center mb-1 md:mb-0 pr-4">Full Name</label>
-                    </div>
-                    <div className="md:w-2/3">
-                        <input onChange={getNameText} value={nameText} className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-full-name" type="text" placeholder="Jane Doe" />
-                    </div>
+        <form className="w-full max-w-sm form-container">
+            <div className="md:flex md:items-center mb-6">
+                <div className="md:w-1/3">
+                    <label className="block text-gray-500 font-bold md:text-center mb-1 md:mb-0 pr-4">Full Name</label>
                 </div>
-                <div className="md:flex md:items-center mb-6">
-                    <div className="md:w-1/3">
-                        <label className="block text-gray-500 font-bold md:text-center mb-1 md:mb-0 pr-4">Password</label>
-                    </div>
-                    <div className="md:w-2/3">
-                        <input onChange={getPasswordText} value={passwordText} className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-password" type="password" placeholder="******************" />
-                    </div>
+                <div className="md:w-2/3">
+                    <input onChange={getNameText} value={nameText} className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-full-name" type="text" placeholder="Jane Doe" />
                 </div>
-                <div className="md:flex md:items-left mb-7">
-                    <div className="md:w-1/4"></div>
-                    <label className="md:w-2/3 block text-gray-500 font-bold form-checkbox">
-                        <input className="mr-2 leading-tight" type="checkbox" />
-                        <span className="text-sm">Confirm terms and conditions!</span>
-                    </label>
+            </div>
+            <div className="md:flex md:items-center mb-6">
+                <div className="md:w-1/3">
+                    <label className="block text-gray-500 font-bold md:text-center mb-1 md:mb-0 pr-4">Password</label>
                 </div>
-                <div className="md:flex md:items-center">
-                    <div className="md:w-1/2"></div>
-                    <div className="md:w-2/3">
-                        <Link onClick={submitPerson} href={{
-                            pathname: "/person-details",
-                            query: {
-                                name: nameText.trim(),
-                                password: passwordText.trim(),
-                            }
-                        }} className={bookingBtnActive()} type="button">Submit</Link>
-                    </div>
+                <div className="md:w-2/3">
+                    <input onChange={getPasswordText} value={passwordText} className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-password" type="password" placeholder="******************" />
                 </div>
-            </form>
+            </div>
+            <div className="md:flex md:items-left mb-7">
+                <div className="md:w-1/4"></div>
+                <label className="md:w-2/3 block text-gray-500 font-bold form-checkbox">
+                    <input className="mr-2 leading-tight" type="checkbox" />
+                    <span className="text-sm">Confirm terms and conditions!</span>
+                </label>
+            </div>
+            <div className="md:flex md:items-center">
+                <div className="md:w-1/2"></div>
+                <div className="md:w-2/3">
+                    <Link onClick={submitPerson} href={{
+                        pathname: "/person-details",
+                        query: {
+                            name: nameText.trim(),
+                            password: passwordText.trim(),
+                        }
+                    }} className={bookingBtnActive()} type="button">Submit</Link>
+                </div>
+            </div>
+        </form>
     );
 }
