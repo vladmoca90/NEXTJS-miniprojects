20724e68277fc7a4e02b4fbe58e6e04f9adc6763
Vlad Mocanu <vladmoca90@gmail.com>
2024-02-04 18:26:25 +0000

    Bug fixed. An onChange event is needed.

--- miniprojects/src/app/page.tsx
+++ miniprojects/src/app/page.tsx
@@ -1,13 +1,21 @@
 "use client";
+import { ChangeEvent, useCallback, useEffect, useState } from "react";
 import "./styles/send-message.css";
 
-export default function sendMessage() {
+export default function SendMessage() {
+    const [message, setMessage] = useState("");
+
+    const getMessage = useCallback(async (event: { target: { value: string; } }) => {
+        setMessage(event.target.value);
+    }, []);
+
     return (
         <div id="sendText">
             <form className="max-w-sm mx-auto">
                 <label htmlFor="message" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your message</label>
-                <textarea id="message" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Leave a comment..."></textarea>
+                <textarea onChange={getMessage} id="message" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Leave a comment..."></textarea>
             </form>
+            <span id="yourText">{message}</span>
         </div>
     );
 }
