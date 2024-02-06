a3e85626481f6fde35dc2fdfb20310dff2b6980d
Vlad Mocanu <vladmoca90@gmail.com>
2024-02-06 21:14:09 +0000

    Trying to recreate the Repository but in all one file

--- miniprojects/src/app/api/appointment/route.ts
+++ miniprojects/src/app/api/appointment/route.ts
@@ -1,19 +1,32 @@
 import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
+import { NextRequest } from "next/server";
 import { allAppointments } from "../../../../lib/appointment/allAppointments";
 
 export async function GET(request: NextRequest) {
-    return NextResponse.json(
-        {
-            body: allAppointments,
-            path: request.nextUrl.pathname,
-            query: request.nextUrl.search,
-            cookies: request.cookies.getAll(),
-        },
-        {
-            status: 200
-        },
-    );
+    const data = await request.json();
+
+    const appointment = allAppointments.find((appointment) => appointment.firstName === data.firstName && appointment.lastName === data.lastName);
+
+    if (appointment === null) {
+        return NextResponse.json({},
+            {
+                status: 404
+            },
+        );
+    } else {
+        return NextResponse.json(
+            {
+                body: appointment,
+                path: request.nextUrl.pathname,
+                query: request.nextUrl.search,
+                cookies: request.cookies.getAll(),
+            },
+            {
+                status: 200
+            },
+        );
+    }
+
 }
 
 // http://localhost:3000/api/appointment
