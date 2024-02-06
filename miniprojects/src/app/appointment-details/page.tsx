11832bdcc25e7ccd63de525102999c04758e7391
Vlad Mocanu <vladmoca90@gmail.com>
2024-02-06 20:38:43 +0000

    Created the appointment-details page

--- miniprojects/src/app/appointment-details/page.tsx
+++ miniprojects/src/app/appointment-details/page.tsx
@@ -1,11 +1,62 @@
 "use client";
+import { useCallback, useEffect, useState } from "react";
 import "./../styles/appointment-details.css";
+import { Appointment } from "../../../lib/appointment/Appointment";
 
 export default function AppointmentDetails({ searchParams }: {
     searchParams: {
-        name: string,
-        code: number,
+        "firstName": string,
+        "lastName": string,
+        "email": string,
+        "password": string,
+        "phone": string,
+        "workplace": string,
     }
 }) {
+    let appointmentsUrl = "http://localhost:3000/api/appointment";
 
+    const [appDetails, setAppDetails] = useState<Appointment>([] as any);
+
+    const getAppDetails = useCallback(async () => {
+        await fetch(appointmentsUrl, {
+            method: "POST",
+            body: JSON.stringify({
+                "firstName": searchParams.firstName,
+                "lastName": searchParams.lastName,
+                "email": searchParams.email,
+                "password": searchParams.password,
+                "phone": searchParams.phone,
+                "workplace": searchParams.workplace,
+            })
+        });
+    }, [appointmentsUrl, searchParams.email, searchParams.firstName, searchParams.lastName, searchParams.password, searchParams.phone, searchParams.workplace]);
+
+    useEffect(() => {
+        getAppDetails();
+    }, [getAppDetails]);
+
+    return (
+        <table className="table-auto">
+            <thead>
+                <tr>
+                    <th>First name</th>
+                    <th>Last name</th>
+                    <th>Email</th>
+                    <th>Password</th>
+                    <th>Phone</th>
+                    <th>Workplace</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>{appDetails.firstName}</td>
+                    <td>{appDetails.lastName}</td>
+                    <td>{appDetails.email}</td>
+                    <td>{appDetails.password}</td>
+                    <td>{appDetails.phone}</td>
+                    <td>{appDetails.workplace}</td>
+                </tr>
+            </tbody>
+        </table>
+    );
 }
