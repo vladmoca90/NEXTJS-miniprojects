59ca458032b106e1833eb904921a1aad0c460b87
Vlad Mocanu <vladmoca90@gmail.com>
2024-02-06 22:23:19 +0000

    Deleted the nationalities dropdown in the end

--- miniprojects/src/app/page.tsx
+++ miniprojects/src/app/page.tsx
@@ -95,11 +95,6 @@ export default function AppointmentForm() {
                         <label htmlFor="floating_company" className="peer-focus:font-medium absolute text-sm  duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Workplace</label>
                     </div>
                 </div>
-                <div className="relative z-0 w-full mb-5 group">
-                    <select id="carPrice" title="carPrice" className="text-gray-900 block py-2.5 px-0 w-full text-sm bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer">
-                        <option value="price" className="text-gray-900 peer-focus:font-medium absolute text-sm duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Select nationality your nationality</option>
-                    </select>
-                </div>
                 <Link href={{
                     pathname: "/appointment-details",
                     query: {
