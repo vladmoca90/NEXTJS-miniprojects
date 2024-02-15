b9e5a4ab5fd1cd02c0c3d454ccc9abbcf031d8ad
Vlad Mocanu <vladmoca90@gmail.com>
2024-02-15 20:09:49 +0000

    Deleted the validation for the name

--- miniprojects/src/app/page.tsx
+++ miniprojects/src/app/page.tsx
@@ -23,14 +23,6 @@ export default function FormPerson() {
         setIsChecked(!isChecked);
     }, [isChecked]);
 
-    const getNameCheck = useCallback(() => {
-        if(nameText.length === 0) {
-            return `validation-match`;
-        } else {
-            return 'validation-match validation-alert';
-        }
-    }, [nameText.length]);
-
     const getPasswordCheck= useCallback(() => {
         if (passwordText.length === 0 || passwordText.match(passValid)) {
             return `validation-match`;
@@ -73,7 +65,6 @@ export default function FormPerson() {
                 </div>
                 <div className="md:w-2/3">
                     <input onChange={getNameText} value={nameText} className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-full-name" type="text" placeholder="Your name" />
-                    <span className={getNameCheck()}>The name is not valid</span>
                 </div>
             </div>
             <div className="md:flex md:items-center mb-6">
