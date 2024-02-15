02faa9cec7e8e9f683a1c3cc0ed7c57b3069a92f
Vlad Mocanu <vladmoca90@gmail.com>
2024-02-15 20:58:59 +0000

    Created the basics for the Countries project

---
+++ miniprojects/src/app/api/countries/route.ts
@@ -0,0 +1,19 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+import { allCountries } from "../../../../lib/countries/allCountries";
+
+export async function GET(request: NextRequest) {
+    return NextResponse.json(
+        {
+            body: allCountries,
+            path: request.nextUrl.pathname,
+            query: request.nextUrl.search,
+            cookies: request.cookies.getAll(),
+        },
+        {
+            status: 200
+        },
+    );
+}
+
+// http://localhost:3000/api/countries
