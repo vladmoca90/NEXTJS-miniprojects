6a1f19ff8813109a55ea01d7625e6c532fb90433
Vlad Mocanu <vladmoca90@gmail.com>
2024-02-17 04:12:13 +0000

    Deleted all and will rewrite

--- miniprojects/src/app/api/cookies/banner/route.ts
+++ miniprojects/src/app/api/cookies/banner/route.ts
@@ -1,27 +1,9 @@
+"use server";
 import { NextRequest, NextResponse } from "next/server";
+import { cookies } from "next/headers";
 
 export async function GET(request: NextRequest) {
-    const bannerCookie = request.cookies.get("bannerCookie");
-
-    if (bannerCookie?.value === "true") {
-        return NextResponse.json({
-            message: "This is the cookie banner!"
-        });
-    }
-
-    const response = NextResponse.json({
-        message: "My cookie banner"
-    });
-
-    response.cookies.set("bannerCookie", "true");
-
-    // response.cookies.set("viewedWelcomeMessage", "true", { // additional cookie options
-    //     httpOnly: true,
-    //     maxAge: 60 * 10,
-    //     secure: true,
-    // });
-
-    return response;
+    
 }
 
 // http://localhost:3000/api/cookies/banner
