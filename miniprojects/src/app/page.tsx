7d087a8e5d53cce519832625786e73a7a05159df
Vlad Mocanu <vladmoca90@gmail.com>
2024-02-17 21:27:01 +0000

    Still testing the cookie set up

--- miniprojects/src/app/page.tsx
+++ miniprojects/src/app/page.tsx
@@ -1,20 +1,45 @@
 "use client";
 import "./styles/cookieButtons.css";
+import { useCallback, useEffect, useState } from "react";
 import Cookies from "js-cookie";
 
 export default function ButtonMain() {
-    const loginBtn = async () => {
-        Cookies.set("name", "VladMocanu");
-    };
+    const [isLogin, setIsLogin] = useState(false);
+
+    useEffect(() => {
+        setIsLogin(true);
+    }, []);
+
+    const setLogin = useCallback(async () => {
+        Cookies.set("name", "VladMocanu", {
+            expires: 2,
+        });
+    }, []);
+
+    // const getLogin = useCallback(async () => {
+    //     const loginValue = Cookies.get("name");
+    // }, []);
+
+    if (!isLogin) {
+        return null;
+    }
 
     return (
         <div id="buttonContent" className="inline-flex">
-            <button type="button" onClick={loginBtn} className="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-8 py-3 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">
+            <button type="button" onClick={setLogin} className="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-8 py-3 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">
                 Login
             </button>
             <button type="button" className="focus:outline-none border border-gray-300 text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-8 py-3 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">
                 Logout
             </button>
+
+            {Cookies.get("name") && (
+                <div className="flex items-center justify-center flex-col gap-3">
+                    <p className="mt-5 text-xl">
+                        {"user-pref" + ": " + Cookies?.get("user-pref")}
+                    </p>
+                </div>
+            )}
         </div>
     );
 }
