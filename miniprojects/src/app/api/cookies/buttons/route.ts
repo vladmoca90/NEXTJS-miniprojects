232dfb146fd39a0e7ec8b190a1dbb8485fea0fcf
Vlad Mocanu <vladmoca90@gmail.com>
2024-02-17 20:23:01 +0000

    Installed js-cookie

--- miniprojects/src/app/api/cookies/buttons/route.ts
+++ miniprojects/src/app/api/cookies/buttons/route.ts
@@ -2,10 +2,17 @@ import { NextResponse, NextRequest } from "next/server";
 import { cookies } from "next/headers";
 
 export async function GET(request: NextRequest) {
-    cookies().set("name", "Vlad");
+    const oneDay = 24 * 60 * 60 * 1000;
+
+    cookies().set("name", "Vlad", {
+        expires: Date.now() - oneDay,
+    });
+
+    const cookieVal = cookies().get("name")?.value;
 
     return NextResponse.json(
         {
+            cookieVal,
             cookies: request.cookies.getAll(),
             success: true,
         },
