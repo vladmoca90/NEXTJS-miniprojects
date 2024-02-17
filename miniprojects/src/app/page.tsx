0b4dafd4eb1472d93d3aa453f1dced16faf395e9
Vlad Mocanu <vladmoca90@gmail.com>
2024-02-17 00:39:27 +0000

    Fixed all the bugs with the compiling

--- miniprojects/src/app/page.tsx
+++ miniprojects/src/app/page.tsx
@@ -1,7 +1,14 @@
-"use server";
+"use client";
 import Link from "next/link";
+import { useCallback, useState } from "react";
 
-export default async function Banner() {
+export default function Banner() {
+    const [closed, setClosed] = useState(false);
+
+    const closeBanner = useCallback(() => {
+        setClosed(closed);
+    },[closed]);
+
     return (
         <div className="flex min-h-screen items-center justify-center">
             <div>
@@ -18,7 +25,7 @@ export default async function Banner() {
                                 </span>
                             </p>
                         </div>
-                        <Link href="##" className="relative align-middle select-none font-sans font-medium text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none w-8 max-w-[32px] h-8 max-h-[32px] rounded-lg text-xs text-white hover:bg-white/10 active:bg-white/30 !absolute top-3 right-3" type="button">
+                        <Link href="##" onClick={closeBanner} className="relative align-middle select-none font-sans font-medium text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none w-8 max-w-[32px] h-8 max-h-[32px] rounded-lg text-xs text-white hover:bg-white/10 active:bg-white/30 !absolute top-3 right-3" type="button">
                             <span className="absolute top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2">
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="h-6 w-6" strokeWidth="2">
                                     <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12"></path>
