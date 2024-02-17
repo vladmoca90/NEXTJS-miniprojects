289c6223fe7fed65ed74c1c58c000e1433ee025c
Vlad Mocanu <vladmoca90@gmail.com>
2024-02-17 03:45:18 +0000

    Created the basics of the cookie banner

--- miniprojects/src/app/api/banner/route.ts
+++ miniprojects/src/app/api/banner/route.ts
@@ -1,6 +1,20 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { cookies } from "next/headers";
 
-export async function GET() {
+export async function GET(request: NextRequest) {
+    const bannerCookie = request.cookies.get("bannerCookie");
 
+    if (bannerCookie?.value === "true") {
+        return NextResponse.json({
+            message: "This is the cookie banner!"
+        });
+    }
+
+    const response = NextResponse.json({
+        message: "My cookie banner"
+    });
+
+    response.cookies.set("bannerCookie", "true");
+
+    return response;
 }
--- miniprojects/src/app/page.tsx
+++ miniprojects/src/app/page.tsx
@@ -15,7 +15,6 @@ export default function Banner() {
                 <div className="grid min-h-[140px] w-full place-items-center overflow-x-scroll rounded-lg p-6 lg:overflow-visible">
                     {!isOpened && (
                         <div role="alert" className="relative block w-full text-base font-regular px-4 py-4 rounded-lg bg-red-500 text-white flex">
-
                             <div className=" mr-12">
                                 <p className="font-bold text-black">
                                     🌟 Black Friday Sale <span className="text-white"> - Up to 50% off on fashion items!</span>
