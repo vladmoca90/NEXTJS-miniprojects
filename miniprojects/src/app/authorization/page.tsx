341e71e612007fcee2b0a8293dced01f1122e33c
Vlad Mocanu <vladmoca90@gmail.com>
2024-02-18 21:41:52 +0000

    Installed auth beta

---
+++ miniprojects/src/app/authorization/page.tsx
@@ -0,0 +1,11 @@
+import { authenticate } from "./../../../lib/authenticate/authenticate";
+ 
+export default function Page() {
+  return (
+    <form action={authenticate}>
+      <input type="email" name="email" placeholder="Email" required />
+      <input type="password" name="password" placeholder="Password" required />
+      <button type="submit">Login</button>
+    </form>
+  )
+}
