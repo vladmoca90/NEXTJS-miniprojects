44c834a6781185426c7e9a1829f37e801e2bfe73
Vlad Mocanu <vladmoca90@gmail.com>
2024-02-18 21:17:14 +0000

    Installed flowbite icons

--- miniprojects/src/app/page.tsx
+++ miniprojects/src/app/page.tsx
@@ -63,6 +63,10 @@ export default function FormPerson() {
 
     return (
         <form className="w-full max-w-sm form-container">
+            <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
+    <path fill-rule="evenodd" d="M7 2a2 2 0 0 0-2 2v1a1 1 0 0 0 0 2v1a1 1 0 0 0 0 2v1a1 1 0 1 0 0 2v1a1 1 0 1 0 0 2v1a1 1 0 1 0 0 2v1c0 1.1.9 2 2 2h11a2 2 0 0 0 2-2V4a2 2 0 0 0-2-2H7Zm3 8a3 3 0 1 1 6 0 3 3 0 0 1-6 0Zm-1 7a3 3 0 0 1 3-3h2a3 3 0 0 1 3 3c0 .6-.4 1-1 1h-6a1 1 0 0 1-1-1Z" clip-rule="evenodd"/>
+  </svg>
+  
             <div className="md:flex md:items-center mb-6">
                 <div className="md:w-1/3">
                     <label className="block text-gray-500 font-bold md:text-center mb-1 md:mb-0 pr-4">Full Name</label>
