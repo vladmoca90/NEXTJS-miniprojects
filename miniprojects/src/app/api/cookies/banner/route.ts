f125f510afe93dc2403c1bcad0e5e0d9bcf5a54a
Vlad Mocanu <vladmoca90@gmail.com>
2024-02-18 01:20:17 +0000

    Made the cookie to expire on 4th Jan 2025

--- miniprojects/src/app/api/cookies/banner/route.ts
+++ miniprojects/src/app/api/cookies/banner/route.ts
@@ -5,6 +5,7 @@ import { NextRequest, NextResponse } from "next/server";
 export async function GET(request: NextRequest) {
     const bannerCookie = cookies().set("name", "closeBanner", {
         httpOnly: true,
+        expires: new Date("2025-01-04"),
         secure: process.env.NODE_ENV === "production",
         sameSite: "strict",
         path: "/",
--- miniprojects/src/app/page.tsx
+++ miniprojects/src/app/page.tsx
@@ -3,7 +3,13 @@ import Link from "next/link";
 import { useCallback, useState } from "react";
 
 export default function Banner() {
+    const bannerUrl = "http://localhost:3000/api/cookies/banner/";
+
     const [isOpened, setIsOpened] = useState(false);
+
+    const getBanner = useCallback(async() => {
+        await fetch(bannerUrl);
+    },[bannerUrl]);
 
     const closeBanner = useCallback(() => {
         setIsOpened(!isOpened);
@@ -26,7 +32,7 @@ export default function Banner() {
                                     </span>
                                 </p>
                             </div>
-                            <Link href="" onClick={closeBanner} className="relative align-middle select-none font-sans font-medium text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none w-8 max-w-[32px] h-8 max-h-[32px] rounded-lg text-xs text-white hover:bg-white/10 active:bg-white/30 !absolute top-3 right-3" type="button">
+                            <Link href="" onClick={getBanner} className="relative align-middle select-none font-sans font-medium text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none w-8 max-w-[32px] h-8 max-h-[32px] rounded-lg text-xs text-white hover:bg-white/10 active:bg-white/30 !absolute top-3 right-3" type="button">
                                 <span className="absolute top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2">
                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="h-6 w-6" strokeWidth="2">
                                         <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12"></path>
