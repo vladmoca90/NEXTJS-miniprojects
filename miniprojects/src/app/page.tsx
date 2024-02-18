0284fe8cd92f560a49f0a5b738c67e2b33617e9a
Vlad Mocanu <vladmoca90@gmail.com>
2024-02-18 19:12:45 +0000

    Reversed all back for now. Will create a copy with Client component

--- miniprojects/src/app/page.tsx
+++ miniprojects/src/app/page.tsx
@@ -1,28 +1,23 @@
 "use client";
 import Link from "next/link";
-import Cookies from "js-cookie";
 import { useCallback, useEffect, useState } from "react";
 
 export default function Banner() {
+    const bannerUrl = "http://localhost:3000/api/cookies/banner";
+
     const [isOpened, setIsOpened] = useState(false);
 
+    const getBanner = useCallback(async () => {
+        await fetch(bannerUrl);
+    }, [bannerUrl]);
+
+    const closeBanner = useCallback(() => {
+        setIsOpened(!isOpened);
+    }, [isOpened]);
+
     useEffect(() => {
-        setIsOpened(true);
-    }, []);
-
-    const bannerCookie = useCallback(() => {
-        Cookies.set("name", "closeBanner", {
-            httpOnly: true,
-            expires: new Date("2025-01-04"),
-            secure: process.env.NODE_ENV === "production",
-            sameSite: "strict",
-            path: "/",
-        });
-    }, []);
-
-    if (!isOpened) {
-        return null;
-    }
+        getBanner();
+    }, [getBanner]);
 
     return (
         <div className="flex min-h-screen items-center justify-center">
@@ -42,7 +37,7 @@ export default function Banner() {
                                         </span>
                                     </p>
                                 </div>
-                                <Link href="" onClick={bannerCookie} className="relative align-middle select-none font-sans font-medium text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none w-8 max-w-[32px] h-8 max-h-[32px] rounded-lg text-xs text-white hover:bg-white/10 active:bg-white/30 !absolute top-3 right-3" type="button">
+                                <Link href="" onClick={closeBanner} className="relative align-middle select-none font-sans font-medium text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none w-8 max-w-[32px] h-8 max-h-[32px] rounded-lg text-xs text-white hover:bg-white/10 active:bg-white/30 !absolute top-3 right-3" type="button">
                                     <span className="absolute top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2">
                                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="h-6 w-6" strokeWidth="2">
                                             <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12"></path>
