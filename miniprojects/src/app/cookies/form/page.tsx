71a4ba6adb6f30729b86d3d177b9ebc73c97e6f1
Vlad Mocanu <vladmoca90@gmail.com>
2024-02-18 19:21:12 +0000

    Rewrote the form again as server side

--- miniprojects/src/app/cookies/form/page.tsx
+++ miniprojects/src/app/cookies/form/page.tsx
@@ -1,25 +1,68 @@
-"use client";
-import { useCallback } from "react";
+import { cookies } from "next/headers";
 
-export default function CookieForm() {
-    const formUrl = "http://localhost:3000/api/cookies/form";
+export default function Home() {
+    const handleLogin = async (formData: FormData) => {
+        "use server";
 
-    const getFormCookie = useCallback(async () => {
-        await fetch(formUrl);
-    }, []);
+        const email = String(formData.get("email"));
+        const password = String(formData.get("password"));
+
+        // perform database operations
+
+        // get the auth token
+
+        cookies().set("auth-token", "some-auth-token-by-server-comp", {
+            maxAge: 48 * 60 * 60,
+            secure: process.env.NODE_ENV === "production",
+            httpOnly: true,
+        });
+    };
+
+    const fetchUserInfo = async () => {
+        const authToken = cookies().get("auth-token")?.value;
+
+        //perform further action
+    };
 
     return (
-        <main className="flex mt-5 flex-col items-center justify-center">
+        <main className="flex mx-3 flex-col items-center justify-center">
             <h1 className="my-5 text-center text-2xl font-bold">
                 Getting and Setting Cookies in Next.js
             </h1>
+
             <div>
-                <h2 className="m-5 text-center text-3xl font-semibold">Set Cookie</h2>
-                <form action={getFormCookie}>
-                    <input type="email" className="bg-transparent px-2 py-2 block my-2 border-2 border-slate-800 rounded-md" placeholder="email" name="email" />
-                    <input type="password" className="bg-transparent px-2 py-2 block my-2 border-2 border-slate-800 rounded-md" placeholder="password" name="password" />
-                    <button className="bg-blue-600 rounded-md px-6 py-2 w-full text-white">Login</button>
+                <h2 className="mt-10 text-center text-3xl font-semibold">Set Cookie</h2>
+                <form action={handleLogin}>
+                    <input
+                        type="email"
+                        className="bg-transparent px-2 py-2 block my-2 border-2 border-slate-800 rounded-md"
+                        placeholder="email"
+                        name="email"
+                    />
+                    <input
+                        type="password"
+                        className="bg-transparent px-2 py-2 block my-2 border-2 border-slate-800 rounded-md"
+                        placeholder="password"
+                        name="password"
+                    />
+                    <button className="bg-blue-600 rounded-md px-6 py-2 w-full text-white">
+                        Login
+                    </button>
                 </form>
+            </div>
+
+            <div className="mt-5">
+                <h2 className="mt-10 text-center text-3xl font-semibold">
+                    {" "}
+                    Cookie Value
+                </h2>
+                {cookies().get("auth-token")?.name && (
+                    <p className="mt-5 text-xl">
+                        {cookies().get("auth-token")?.name +
+                            ": " +
+                            cookies()?.get("auth-token")?.value}
+                    </p>
+                )}
             </div>
         </main>
     );
