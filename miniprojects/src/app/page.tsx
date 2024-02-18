9b773d0a5ee371156916dcd437c6b5a9db78f373
Vlad Mocanu <vladmoca90@gmail.com>
2024-02-18 19:49:42 +0000

    Cookie appears when clicking on the X

--- miniprojects/src/app/page.tsx
+++ miniprojects/src/app/page.tsx
@@ -7,24 +7,28 @@ export default function Banner() {
 
     const [isOpened, setIsOpened] = useState(false);
 
-    useEffect(() => {
-        setIsOpened(true);
-    }, []);
-
     const getBanner = useCallback(async () => {
         await fetch(bannerUrl);
     }, [bannerUrl]);
 
-    const closeBanner = useCallback(() => {
-        setIsOpened(!isOpened);
-    }, [isOpened]);
+    useEffect(() => {
+        setIsOpened(true);
+    }, []);
+
+    // const closeBanner = useCallback(() => {
+    //     setIsOpened(!isOpened);
+    // }, [isOpened]);
+
+    if(!isOpened) {
+        return null;
+    }
 
     return (
         <div className="flex min-h-screen items-center justify-center">
             <div>
                 <div className="grid min-h-[140px] w-full place-items-center overflow-x-scroll rounded-lg p-6 lg:overflow-visible">
                     {
-                        !isOpened && (
+                        isOpened && (
                             <div role="alert" className="relative block w-full text-base font-regular px-4 py-4 rounded-lg bg-red-500 text-white flex">
                                 <div className=" mr-12">
                                     <p className="font-bold text-black">
@@ -37,7 +41,7 @@ export default function Banner() {
                                         </span>
                                     </p>
                                 </div>
-                                <Link href="" onClick={closeBanner} className="relative align-middle select-none font-sans font-medium text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none w-8 max-w-[32px] h-8 max-h-[32px] rounded-lg text-xs text-white hover:bg-white/10 active:bg-white/30 !absolute top-3 right-3" type="button">
+                                <Link href="" onClick={getBanner} className="relative align-middle select-none font-sans font-medium text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none w-8 max-w-[32px] h-8 max-h-[32px] rounded-lg text-xs text-white hover:bg-white/10 active:bg-white/30 !absolute top-3 right-3" type="button">
                                     <span className="absolute top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2">
                                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="h-6 w-6" strokeWidth="2">
                                             <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12"></path>
