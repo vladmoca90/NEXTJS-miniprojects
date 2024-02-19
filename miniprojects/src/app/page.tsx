52d91730251a8e650626845417d312045f7bc38d
Vlad Mocanu <vladmoca90@gmail.com>
2024-02-19 19:14:48 +0000

    Deleted in the end the .countries-statistics

--- miniprojects/src/app/page.tsx
+++ miniprojects/src/app/page.tsx
@@ -26,11 +26,6 @@ export default function CountriesList() {
 
     return (
         <div>
-            <div className="countries-statistics">
-                <div>Total number of countries is: {countries.length}</div>
-                <div>Total number of countries is: {countries.length}</div>
-                <div>Total number of countries is: {countries.length}</div>
-            </div>
             <div className="countries-table">
                 <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
                     <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
