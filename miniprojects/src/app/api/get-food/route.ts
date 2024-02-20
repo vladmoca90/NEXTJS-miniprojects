04fe37ee8fdf1451b3c5ba5323fa1bf3da9f4d88
Vlad Mocanu <vladmoca90@gmail.com>
2024-02-20 22:27:03 +0000

    Build the food next page but still have a few bugs to fix

---
+++ miniprojects/src/app/api/get-food/route.ts
@@ -0,0 +1,37 @@
+import { NextRequest, NextResponse } from "next/server";
+import { allFoods } from "../../../../lib/foodList/allFoods";
+
+export async function GET(request: NextRequest) {
+    const foodName = request.nextUrl.searchParams.get("foodName");
+
+    if (foodName === null) {
+        return NextResponse.json(
+            {},
+            {
+                status: 400,
+            }
+        );
+    }
+
+    const food = allFoods.find((food) => {
+        return foodName === food.name;
+    });
+
+    if (!food) {
+        return NextResponse.json(
+            {},
+            {
+                status: 404,
+            }
+        );
+    } else {
+        return NextResponse.json(
+            {
+                food,
+            },
+            {
+                status: 200,
+            }
+        );
+    }
+}
