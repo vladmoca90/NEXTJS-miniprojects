60c0afd2c3b28c4b1817dff49b9d9a0f877cf38f
Vlad Mocanu <vladmoca90@gmail.com>
2024-02-22 20:20:08 +0000

    Test

--- miniprojects/src/app/page.tsx
+++ miniprojects/src/app/page.tsx
@@ -1,83 +1,103 @@
 "use client";
-import "./styles/wines.css";
-import Image from "next/image";
+import "./styles/countries.css";
 import Link from "next/link";
-import { Wine } from "../../lib/wines/Wine";
-import { useCallback, useEffect, useState } from "react";
+import { Country } from "../../lib/countries/Country";
+import { CountryNameOrCode } from "../../lib/countries/countryNameOrCode";
+import { ChangeEvent, useCallback, useEffect, useState } from "react";
 
-export default function WinesSell() {
-    let winesUrl = "http://localhost:3000/api/wines";
+export default function CountriesList() {
+    let countriesUrl = "http://localhost:3000/api/countries";
 
-    const [wines, setWines] = useState<Wine[]>([]);
-    const [selectedWine, setSelectedWine] = useState<Wine[]>([]);
+    const [countries, setCountries] = useState<Country[]>([]);
+    const [countryNameOrCode, setCountryNameOrCode] = useState("");
+    const [results, setResults] = useState<CountryNameOrCode[]>([]);
 
-    const getWines = useCallback(async () => {
-        const res = await fetch(winesUrl);
+    const getCountries = useCallback(async () => {
+        const res = await fetch(countriesUrl);
 
         if (!res.ok) {
-            throw new Error("Failed to fetch data");
+            throw new Error("The data could not be fetched!");
         }
 
         const data = await res.json();
 
-        setWines(data.body);
-    }, [winesUrl]);
+        setCountries(data.body);
+    }, [countriesUrl]);
 
-    const getSelectedWine = useCallback(async (e: { target: { value: string } }) => {
+    const getSelectedCountry = useCallback(async (e: ChangeEvent<HTMLInputElement>) => {
         const value = e.target.value;
 
-        if (value === "All products") {
-            setSelectedWine(wines);
+        setCountryNameOrCode(value);
+
+        if (value.length > 2) {
+            const res = await fetch(countriesUrl);
+
+            if (!res.ok) {
+                throw new Error("The data could not be fetched!");
+            }
+
+            const data = await res.json();
+
+            setResults(data.result);
         } else {
-            const searchWine = wines.filter((wine) => {
-                return value === wine.name;
-            });
-            setSelectedWine(searchWine);
+            return setResults([]);
         }
-    }, [wines]);
+    }, [countriesUrl]);
 
     useEffect(() => {
-        getWines();
-    }, [getWines]);
+        getCountries();
+    }, [getCountries]);
 
     return (
-        <section className="box">
-            <div>
-                <select id="productsList" title="wines" onChange={getSelectedWine}>
-                    <option value="All products">All products</option>
+        <div>
+            <div className="countries-search">
+                <label className="countries-search-title">Search countries:</label>
+                <input onChange={getSelectedCountry} className="countries-search-bar" title="search" name="search" type="text" placeholder="Search countries..." />
+                <ul id="searchCountries">
                     {
-                        wines.map((wine, index) => {
+                      results.map((result, index) => {
+                        if(result.type === "Name") {
                             return (
-                                <option key={index} value={wine.name}>{wine.name}</option>
+                                <li key={index}>{result.name}</li>
                             );
-                        })
+                        } else {
+                            return (
+                                <li key={index}>{result.code}</li>
+                            );
+                        }
+                      })
                     }
-                </select>
+                </ul>
             </div>
-            <div className="products-container">
-                {
-                    selectedWine.map((wine, index) => {
-                        return (
-                            <div className="product" key={index}>
-                                <div className="product-description__top">
-                                    <p className="product-title">{wine.name}</p>
-                                </div>
-                                <div className="product-description__bottom">
-                                    <Image alt={wine.name} className="product-img" width={200} height={100} key={index} src={wine.img} />
-                                </div>
-                                <div className="wine-link">
-                                    <Link href={{
-                                        pathname: "/wine-details",
-                                        query: {
-                                            "wineName": wine.name,
-                                        }
-                                    }}>Check details</Link>
-                                </div>
-                            </div>
-                        );
-                    })
-                }
+            <div className="countries-table">
+                <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                    <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                        <tr>
+                            <th scope="col">Country name</th>
+                            <th scope="col">Country code</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            countries.map((country, index) => {
+                                return (
+                                    <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700" key={index}>
+                                        <td>
+                                            <Link href={{
+                                                pathname: "/country-name",
+                                                query: {
+                                                    "countryName": country.name,
+                                                },
+                                            }}>{country.name}</Link>
+                                        </td>
+                                        <td>{country.code}</td>
+                                    </tr>
+                                );
+                            })
+                        }
+                    </tbody>
+                </table>
             </div>
-        </section>
+        </div>
     );
 }
