28f40333449626b7b7c104e7d1629ac0aa800687
Vlad Mocanu <vladmoca90@gmail.com>
2024-02-25 20:59:23 +0000

    Renamed a DeleteText the parent folder

--- miniprojects/src/app/api/random/deleteText/route.ts
+++ miniprojects/src/app/api/random/deleteText/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
-import { allTexts } from "../../../../../lib/random/deleteTest/allTexts";
+import { allTexts } from "../../../../../lib/random/deleteText/allTexts";
 
 export async function DELETE(request: NextRequest) {
     return NextResponse.json(
--- miniprojects/src/app/random/deleteText/page.tsx
+++ miniprojects/src/app/random/deleteText/page.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 export default function MyDeleteTest() {
+    const randomTextUrl = "http://localhost:3000/api/random/deleteText";
+    
     return (
         <div>
             <p></p>
