6292cd63092064d89d196e4b71bb8a5b478e79b1
Vlad Mocanu <vladmoca90@gmail.com>
2024-02-26 20:43:15 +0000

    Started to rebuild teh Transactions project

--- miniprojects/src/app/transactions/page.tsx
+++ miniprojects/src/app/transactions/page.tsx
@@ -1,7 +1,52 @@
 "use client";
+import { useCallback, useEffect, useState } from "react";
+import { Transaction } from "../../../lib/transactions/Transaction";
 
 export default function Transactions() {
+    let transactionsUrl = "https://csb-u0slz.vercel.app/api/transactions";
+
+    const [transactions, setTransactions] = useState<Transaction[]>([]);
+    const [getTRansactions, setGetTransactions] = useState<Transaction[]>([]);
+
+    const getTransaction = useCallback(async () => {
+        const res = await fetch(transactionsUrl);
+
+        if (!res.ok) {
+            throw new Error("The data is not valid!");
+        } else {
+            console.log("Data is valid!");
+        }
+
+        const data = await res.json();
+
+        setTransactions(data);
+    }, [transactionsUrl]);
+
+    const getSelectedTransaction = useCallback(() => {
+
+    }, []);
+
+    useEffect(() => {
+        getTransaction();
+    }, [getTransaction]);
+
     return (
-        <div></div>
+        <div id="transaction-container">
+            <span>{"SELECTED_CATEGORY"}</span>
+            {
+                transactions.map((transaction, index) => (
+                    <div className="transactions-content" onClick={getSelectedTransaction} key={index}>
+                        <span className="transaction-date">
+                            {new Date(transaction.date).toLocaleString([], {
+                                hour: "2-digit",
+                                minute: "2-digit",
+                            })}
+                        </span>
+                        <span className="transaction-name">{transaction.name}</span>
+                        <span>£{Math.abs(transaction.amount)}</span>
+                    </div>
+                ))
+            }
+        </div>
     );
 }
