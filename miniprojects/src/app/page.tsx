26e5b4ded37bd6fc566c605fa8278ac53b6d20d6
Vlad Mocanu <vladmoca90@gmail.com>
2024-02-28 20:32:26 +0000

    Added checkboxes for the wines project

--- miniprojects/src/app/page.tsx
+++ miniprojects/src/app/page.tsx
@@ -42,6 +42,18 @@ export default function WinesSell() {
 
     return (
         <section className="box">
+            <div className="flex items-center">
+                {
+                    wines.map((wine, index) => {
+                        return (
+                            <div className="flex items-center mt-5 ml-5" key={index}>
+                                <input id="checkedCheckbox" type="checkbox" value={wine.name} className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
+                                <label htmlFor="checked-checkbox" className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">{wine.name}</label>
+                            </div>
+                        );
+                    })
+                }
+            </div>
             <div>
                 <select id="productsList" title="wines" onChange={getSelectedWine}>
                     <option value="All products">All products</option>
