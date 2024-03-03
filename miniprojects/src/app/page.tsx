a91ad2c0acd4e77f9740d46eb6d7f8b462dc5ef5
Vlad Mocanu <vladmoca90@gmail.com>
2024-03-03 02:42:34 +0000

    Created the sample for the wines checkboxes filterproject

--- miniprojects/src/app/page.tsx
+++ miniprojects/src/app/page.tsx
@@ -1,14 +1,18 @@
 "use client";
-import "./styles/shop.css";
+import "./styles/wines.css";
 import Image from "next/image";
 import Link from "next/link";
-import { ChangeEvent, useCallback, useEffect, useState } from "react";
+import { Wine } from "../../lib/wines/Wine";
+import { useCallback, useEffect, useState } from "react";
 
-export default function ShopProducts() {
-    const shopUrl = "http://localhost:3000/api/shop";
+export default function WinesSell() {
+    const winesUrl = "http://localhost:3000/api/wines";
 
-    const getShop = useCallback(async () => {
-        const res = await fetch(shopUrl);
+    const [wines, setWines] = useState<Wine[]>([]);
+    const [checked, setChecked] = useState(false);
+
+    const getWines = useCallback(async () => {
+        const res = await fetch(winesUrl);
 
         if (!res.ok) {
             throw new Error("The details are NOT valid!");
@@ -17,10 +21,56 @@ export default function ShopProducts() {
         }
 
         const data = await res.json();
-    }, []);
+
+        setWines(data.body);
+    }, [winesUrl]);
+
+    useEffect(() => {
+        getWines();
+    }, [getWines]);
 
     return (
         <section className="box">
+            <div className="wines-checkboxes flex items-center">
+                <div className="flex items-center px-2 py-1 border border-gray-300 rounded dark:border-gray-700">
+                    <input id="bordered-checkbox-1" type="checkbox" value="All wines" name="bordered-checkbox" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
+                    <label htmlFor="bordered-checkbox-1" className="w-full py-4 ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">All wines</label>
+                </div>
+                {
+                    wines.map((wine, index) => {
+                        return (
+                            <div className="flex items-center px-2 py-1 border border-gray-300 rounded dark:border-gray-700" key={index}>
+                                <input id="bordered-checkbox-2" type="checkbox" value={wine.name} name="bordered-checkbox" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
+                                <label htmlFor="bordered-checkbox-2" className="w-full py-4 ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">{wine.name}</label>
+                            </div>
+                        );
+                    })
+                }
+            </div>
+            <div className="products-container">
+                {
+                    wines.map((wine, index) => {
+                        return (
+                            <div className="product" key={index}>
+                                <div className="product-description__top">
+                                    <p className="product-title">{wine.name}</p>
+                                </div>
+                                <div className="product-description__bottom">
+                                    <Image alt={wine.name} className="product-img" width={200} height={100} key={index} src={wine.img} />
+                                </div>
+                                <div className="wine-link">
+                                    <Link href={{
+                                        pathname: "/wine-details",
+                                        query: {
+                                            "wineName": wine.name,
+                                        }
+                                    }}>Check details</Link>
+                                </div>
+                            </div>
+                        );
+                    })
+                }
+            </div>
         </section>
     );
 }
---
+++ miniprojects/src/app/shop/page.tsx
@@ -0,0 +1,26 @@
+"use client";
+import "./styles/shop.css";
+import Image from "next/image";
+import Link from "next/link";
+import { ChangeEvent, useCallback, useEffect, useState } from "react";
+
+export default function ShopProducts() {
+    const shopUrl = "http://localhost:3000/api/shop";
+
+    const getShop = useCallback(async () => {
+        const res = await fetch(shopUrl);
+
+        if (!res.ok) {
+            throw new Error("The details are NOT valid!");
+        } else {
+            console.log("The details are valid!");
+        }
+
+        const data = await res.json();
+    }, []);
+
+    return (
+        <section className="box">
+        </section>
+    );
+}
