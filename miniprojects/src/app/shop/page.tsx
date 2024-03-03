ea7a940671c233d4ca78cdbb2eaaaa01100841d5
Vlad Mocanu <vladmoca90@gmail.com>
2024-03-03 20:58:05 +0000

    Added images and build the basic HTML and data for the shop

--- miniprojects/src/app/shop/page.tsx
+++ miniprojects/src/app/shop/page.tsx
@@ -2,12 +2,15 @@
 import "./styles/shop.css";
 import Image from "next/image";
 import Link from "next/link";
+import { Product } from "../../../lib/shop/Product";
 import { ChangeEvent, useCallback, useEffect, useState } from "react";
 
 export default function ShopProducts() {
     const shopUrl = "http://localhost:3000/api/shop";
 
-    const getShop = useCallback(async () => {
+    const [shops, setShops] = useState<Product[]>([]);
+
+    const getShops = useCallback(async () => {
         const res = await fetch(shopUrl);
 
         if (!res.ok) {
@@ -17,10 +20,38 @@ export default function ShopProducts() {
         }
 
         const data = await res.json();
+
+        setShops(data.body);
     }, []);
+
+    useEffect(() => {
+        getShops();
+    }, [getShops]);
 
     return (
         <section className="box">
+            <div className="shop-search">
+
+            </div>
+            <div className="shop-list">
+                {
+                    shops.map((shop, index) => {
+                        return (
+                            <div className="shop-card" key={index}>
+                            <div className="shop-image">
+                                <Image src={shop.image} alt={shop.name} />
+                            </div>
+                            <div className="shop-details">
+                                <p className="shop-title">{shop.name}</p>
+                                <div>
+        
+                                </div>
+                            </div>
+                        </div>
+                        );
+                    })
+                }
+            </div>
         </section>
     );
 }
