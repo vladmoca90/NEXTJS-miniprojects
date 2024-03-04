b2cbdb744f27f4b9208cdfdfb91b190c6f97bac6
Vlad Mocanu <vladmoca90@gmail.com>
2024-03-04 20:17:58 +0000

    Aligned the widgets and added a part of the data.

--- miniprojects/src/app/page.tsx
+++ miniprojects/src/app/page.tsx
@@ -5,7 +5,7 @@ import { useCallback, useEffect, useState } from "react";
 export default function ProductWidgets() {
     let widgetsUrl = "https://api.mocki.io/v2/016d11e8/product-widgets";
 
-    const [widgets, setWidgets] = useState([]);
+    const [widgets, setWidgets] = useState<[]>([]);
 
     const getWidgets = useCallback(async () => {
         const res = await fetch(widgetsUrl);
@@ -31,20 +31,26 @@ export default function ProductWidgets() {
 
     return (
         <div id="productWidgets">
+            <div className="productWidgets-top">
+                <h3>Per product widgets</h3>
+            </div>
+            <div className="productWidgets-content">
             {
                 widgets.map((widget, index) => {
                     return (
-                        <div className="widget widget--green" key={index}>
+                        <div className="widget widget--blue" key={index}>
                             <div className="widget-top">
-
+                                <p className="widget-title">This product {widget.action}</p>
+                                <p className="widget-subtitle">{widget.amount} {widget.type}</p>
                             </div>
                             <div className="widget-content">
-
+                                
                             </div>
                         </div>
                     );
                 })
             }
+            </div>
         </div>
     );
 }
