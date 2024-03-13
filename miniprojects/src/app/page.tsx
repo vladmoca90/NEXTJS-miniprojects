d3407b34d815261bcba4ff347d721273b9eecf3e
Vlad Mocanu <vladmoca90@gmail.com>
2024-03-13 22:32:06 +0000

    Removed all and will start from scratch

--- miniprojects/src/app/page.tsx
+++ miniprojects/src/app/page.tsx
@@ -9,7 +9,7 @@ export default function WinesSell() {
     const winesUrl = "http://localhost:3000/api/wines";
 
     const [wines, setWines] = useState<Wine[]>([]);
-    const [checked, setChecked] = useState(false);
+    const [checkedWine, setCheckedWine] = useState<Wine[]>([]);
 
     const getWines = useCallback(async () => {
         const res = await fetch(winesUrl);
@@ -25,19 +25,6 @@ export default function WinesSell() {
         setWines(data.body);
     }, [winesUrl]);
 
-    const onCheck = useCallback((e: { target: { checked: boolean } }) => {
-        console.log(e.target.checked);
-        setChecked(e.target.checked);
-    }, []);
-
-    const checkedWines = useCallback((e: MouseEvent<HTMLInputElement>) => {
-        if (!checked) {
-            return wines;
-        } else {
-            return wines.filter((wine) => wine.name);
-        }
-    }, [checked, wines]);
-
     useEffect(() => {
         getWines();
     }, [getWines]);
@@ -46,14 +33,14 @@ export default function WinesSell() {
         <section className="box">
             <div className="wines-checkboxes flex items-center">
                 <div className="flex items-center px-4 py-0 border border-gray-300 rounded dark:border-gray-700">
-                    <input onChange={onCheck} id="bordered-checkbox-1" type="checkbox" value="All wines" name="bordered-checkbox" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-400 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
+                    <input id="bordered-checkbox-1" type="checkbox" value="All wines" name="bordered-checkbox" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-400 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
                     <label htmlFor="bordered-checkbox-1" className="w-full py-4 ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">All wines</label>
                 </div>
                 {
                     wines.map((wine, index) => {
                         return (
                             <div className="flex items-center px-4 py-0 border border-gray-300 rounded dark:border-gray-700" key={index}>
-                                <input onClick={onCheck} id="bordered-checkbox-2" type="checkbox" value={wine.name} name="bordered-checkbox" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-400 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
+                                <input id="bordered-checkbox-2" type="checkbox" value={wine.name} name="bordered-checkbox" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-400 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
                                 <label htmlFor="bordered-checkbox-2" className="w-full py-4 ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">{wine.name}</label>
                             </div>
                         );
@@ -62,7 +49,7 @@ export default function WinesSell() {
             </div>
             <div className="products-container">
                 {
-                    checkedWines().map((wine, index) => {
+                    wines.map((wine, index) => {
                         return (
                             <div className="product" key={index}>
                                 <div className="product-description__top">
