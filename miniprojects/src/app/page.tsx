7d7f2cefa1e062aad7bdcd78ba4a1256357f5dc5
Vlad Mocanu <vladmoca90@gmail.com>
2024-03-29 15:45:40 +0000

    Created a separate component for SelectedTransaction

--- miniprojects/src/app/page.tsx
+++ miniprojects/src/app/page.tsx
@@ -1,8 +1,9 @@
 "use client";
 import "./styles/transactions.css";
 import { Transaction } from "./../../data/transactions/Transaction";
-import { MouseEvent, useCallback, useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import TransactionComponent from "./TransactionComponent";
+import SelectedTransactionComponent from "./SelectedTransactionComponent";
 
 export default function Transactions() {
     let transactionsUrl = "http://localhost:3000/api/transactions";
@@ -42,31 +43,7 @@ export default function Transactions() {
                 {
                     getTransactions.map((getTransaction, index) => {
                         return (
-                            <div className="border-b font-medium dark:border-neutral-500" key={index}>
-                                <table className="min-w-full text-left text-sm font-light text-surface dark:text-white">
-                                    <thead className="border-b font-medium dark:border-neutral-500">
-                                        <tr>
-                                            <th scope="col" className="px-6 py-4">Date</th>
-                                            <th scope="col" className="px-6 py-4">Name</th>
-                                            <th scope="col" className="px-6 py-4">Amount</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        <tr className="border-b dark:border-neutral-500">
-                                            <td className="whitespace-nowrap px-6 py-4">
-                                                {
-                                                    new Date(getTransaction.date).toLocaleString([], {
-                                                        hour: "2-digit",
-                                                        minute: "2-digit",
-                                                    })
-                                                }
-                                            </td>
-                                            <td className="whitespace-nowrap px-6 py-4">{getTransaction.name}</td>
-                                            <td className="whitespace-nowrap px-6 py-4">£{Math.abs(getTransaction.amount)}</td>
-                                        </tr>
-                                    </tbody>
-                                </table>
-                            </div>
+                           <SelectedTransactionComponent getTransaction={getTransaction} key={index} />
                         );
                     })
                 }
