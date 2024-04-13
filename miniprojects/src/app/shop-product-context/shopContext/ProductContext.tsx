f44c4b1a07ec75dcbff0877f6adaf6380baab821
Vlad Mocanu <vladmoca90@gmail.com>
2024-04-13 16:04:31 +0100

    Will remake all from scratch

--- miniprojects/src/app/shop-product-context/shopContext/ProductContext.tsx
+++ miniprojects/src/app/shop-product-context/shopContext/ProductContext.tsx
@@ -1,8 +1,2 @@
 import { createContext } from "react";
 import { Product } from "../../../../data/shop-products/Product";
-
-export interface ProductRepository {
-    product: Product;
-    onCountUpdatedAdd: () => void;
-    onCountUpdatedRemove: () => void;
-}
