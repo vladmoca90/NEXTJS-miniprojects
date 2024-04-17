56f73282d0a20848b9ff057ccbb3697bfa487191
Vlad Mocanu <vladmoca90@gmail.com>
2024-04-17 23:23:42 +0100

    Will return to the transaction again

--- miniprojects/src/app/transactions/transactionContext/TransactionContext.tsx
+++ miniprojects/src/app/transactions/transactionContext/TransactionContext.tsx
@@ -1,10 +1,14 @@
 import { createContext, useContext } from "react";
 import { Transaction } from "../../../../data/transactions/Transaction";
 
-export const TransactionContext = createContext<Transaction | any>(undefined);
+export const TransactionContext = createContext<Transaction | undefined>(undefined);
 
 export function useTransactionContext() {
     const useTransaction = useContext(TransactionContext);
+
+    if (useTransaction === undefined) {
+        throw new Error("The transaction context cannot be undefined!");
+    }
 
     return useTransaction;
 }
