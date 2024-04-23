7e2a4cf465d2b591b9329de81a6b1944e5134940
Vlad Mocanu <vladmoca90@gmail.com>
2024-04-24 00:43:05 +0100

    Added the { wines } only to the third component

--- miniprojects/src/app/FilteredWinesComponentThree.tsx
+++ miniprojects/src/app/FilteredWinesComponentThree.tsx
@@ -2,11 +2,11 @@ import { useContext } from "react";
 import { WineContext } from "./wines-dropdown-context/wineContext/WineContext";
 
 export default function FilteredWinesComponentThree() {
-    const { wines, setWines } = useContext(WineContext);
+    const { wines } = useContext(WineContext);
 
     return (
-        <div>
-            {wines.length}
+        <div className="total-wines-context">
+            <p>Total number of wines is {wines.length}</p>
         </div>
     )
 }
