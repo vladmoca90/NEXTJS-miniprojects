678fb3efd7f883389c044f4a69fe6eee679b5738
Vlad Mocanu <vladmoca90@gmail.com>
2024-04-24 23:44:28 +0100

    Added to make the forename uppercase

--- miniprojects/src/app/page.tsx
+++ miniprojects/src/app/page.tsx
@@ -1,16 +1,17 @@
 "use client";
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import ProfileComponent from "./ProfileComponent";
 import { data, ProfileContext } from "./person-profile/personProfileContext/ProfileContext";
 
 // example you followed: https://chat.openai.com/c/5c08ed3d-7522-4431-93b3-12aacf00015a
 
 export default function ProfileProfile() {
-    const profile = useState(data.forename);
+    const [firstName, setFirstName] = useState(data.forename);
 
-    const spliceForename = (data: any) => {
-        return profile.splice(0, 3);
-    }
+    const getNewFirstName = useCallback(() => {
+        const value = data.forename.toUpperCase();
+        setFirstName(value);
+    }, []);
 
     return (
         <div id="ProfileContent">
