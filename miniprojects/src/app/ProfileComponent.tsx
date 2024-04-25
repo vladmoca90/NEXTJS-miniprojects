69618049d9ae4ec66ee844b5e7480113127f21f3
Vlad Mocanu <vladmoca90@gmail.com>
2024-04-25 01:48:12 +0100

    Renamed data as usedData on the ProfileComponent

--- miniprojects/src/app/ProfileComponent.tsx
+++ miniprojects/src/app/ProfileComponent.tsx
@@ -9,14 +9,14 @@ const profileStyle = {
 }
 
 export default function ProfileComponent() {
-    const data = useContext(ProfileContext);
+    const usedData = useContext(ProfileContext);
 
     return (
         <div style={profileStyle}>
-            <h3>Full name: {data.forename} {data.surname}</h3>
-            <h3>Age: {data.age}</h3>
-            <h3>Nationality: {data.nationality}</h3>
-            <h3>Occupation: {data.occupation}</h3>
+            <h3>Full name: {usedData.forename} {usedData.surname}</h3>
+            <h3>Age: {usedData.age}</h3>
+            <h3>Nationality: {usedData.nationality}</h3>
+            <h3>Occupation: {usedData.occupation}</h3>
         </div>
     );
 }
