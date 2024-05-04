64e3c8dcd3a186ec2e81b3f58546c31b6acb8f3d
Vlad Mocanu <vladmoca90@gmail.com>
2024-05-04 14:24:01 +0100

    Removed the inline styling form the Profile component

--- miniprojects/src/app/ProfileComponent.tsx
+++ miniprojects/src/app/ProfileComponent.tsx
@@ -1,18 +1,11 @@
 import { useContext } from "react";
 import { ProfileContext } from "./person-profile/personProfileContext/ProfileContext";
 
-const profileStyle = {
-    lineHeight: "24px",
-    margin: "2rem auto 0",
-    maxWidth: "320px",
-    padding: "0 0 1rem",
-}
-
 export default function ProfileComponent() {
     const usedData = useContext(ProfileContext);
 
     return (
-        <div style={profileStyle}>
+        <div className="profile-container">
             <h3>Full name: {usedData.forename} {usedData.surname}</h3>
             <h3>Age: {usedData.age}</h3>
             <h3>Nationality: {usedData.nationality}</h3>
