99793a18fa929af4acb14a26d47767bad18a3936
Vlad Mocanu <vladmoca90@gmail.com>
2024-05-18 16:00:53 +0100

    Started to put the data in an external file

---
+++ miniprojects/data/personProfile/profileData.tsx
@@ -0,0 +1,11 @@
+export interface profileData {
+    forename: string,
+    surname: string,
+    age: number,
+    nationality: string,
+    occupation: string,
+}
+
+export const Profile: profileData[] = {
+
+}
--- miniprojects/src/app/person-profile/personProfileContext/ProfileContext.tsx
+++ miniprojects/src/app/person-profile/personProfileContext/ProfileContext.tsx
@@ -1,11 +1,3 @@
 import { createContext } from "react";
 
-export const data = {
-    forename: "Jack",
-    surname: "McGovern",
-    age: 33,
-    nationality: "British",
-    occupation: "Senior Design Producer",
-}
-
 export const ProfileContext = createContext(data);
