7306a7d4448e3ba61f85a567965f095380459468
Vlad Mocanu <vladmoca90@gmail.com>
2024-05-18 15:43:44 +0100

    Testing

--- miniprojects/src/app/shop-product-context/shopContext/ProductContext.tsx
+++ miniprojects/src/app/shop-product-context/shopContext/ProductContext.tsx
@@ -1,7 +1,7 @@
 import { createContext, useContext } from "react";
 import { Product } from "../../../../data/shop-products/Product";
 
-export const ProductContext = createContext<Product>({} as any);
+export const ProductContext = createContext<Product[]>([]);
 
 export function useProductContext() {
     const product = useContext(ProductContext);
