de648345acbb71b80caff9b4b77ffb72cb3002c4
Vlad Mocanu <vladmoca90@gmail.com>
2024-05-18 12:53:05 +0100

    Put the profile back to its folder

--- miniprojects/src/app/person-profile/ProfileComponent.tsx
+++ miniprojects/src/app/person-profile/ProfileComponent.tsx
@@ -1,6 +1,6 @@
 import "./styles/profile.css";
 import { useContext } from "react";
-import { ProfileContext } from "./person-profile/personProfileContext/ProfileContext";
+import { ProfileContext } from "./personProfileContext/ProfileContext";
 
 export default function ProfileComponent() {
     const profile = useContext(ProfileContext);
