ba068e32886619071e14d67a60d4744c55aaa2cf
Vlad Mocanu <vladmoca90@gmail.com>
2024-05-20 22:06:22 +0100

    Created the basics for the sign in and out project with context

---
+++ miniprojects/src/app/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+import "./styles/sign-in-out.css";
+import NavComponent from "./NavComponent";
+
+export default function SignComponent() {
+    return (
+        <div>
+            <NavComponent />
+            <h1>Sign in</h1>
+        </div>
+    );
+}
