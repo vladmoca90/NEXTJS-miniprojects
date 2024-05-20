13ae7e6762404b05352cec5bacaac5742e1a77ce
Vlad Mocanu <vladmoca90@gmail.com>
2024-05-20 21:57:52 +0100

    Deleted the context function

--- miniprojects/src/app/shop-product-context/productContext/ProductContext.tsx
+++ miniprojects/src/app/shop-product-context/productContext/ProductContext.tsx
@@ -2,13 +2,3 @@ import { createContext, useContext } from "react";
 import { Product } from "../../../../data/shop-products/Product";
 
 export const ProductContext = createContext<Product[]>([]);
-
-export function UsedProductContext() {
-    const usedProduct = useContext(ProductContext);
-
-    if (usedProduct === undefined) {
-        throw new Error("The product cannot be undefined!");
-    }
-
-    return usedProduct;
-}
