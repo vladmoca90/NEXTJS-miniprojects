260d13b0fec081ae9466e165495f50e72743fc45
Vlad Mocanu <vladmoca90@gmail.com>
2024-05-23 20:45:40 +0100

    Deleted the conditions from the WineContext

--- miniprojects/src/app/wines-dropdown-context/wineContext/WineContext.tsx
+++ miniprojects/src/app/wines-dropdown-context/wineContext/WineContext.tsx
@@ -1,13 +1,3 @@
 import { createContext, useContext } from "react";
 
 export const WineContext = createContext({});
-
-export function useWineContext() {
-    const wineRepository = useContext(WineContext);
-
-    if(wineRepository === "undefined") {
-        throw new Error("The context cannot be undefined");
-    }
-
-    return wineRepository;
-}
