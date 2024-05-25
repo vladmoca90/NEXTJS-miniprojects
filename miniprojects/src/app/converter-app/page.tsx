1a1135742d6656c488cd4ec89dbd5e92956b23b4
Vlad Mocanu <vladmoca90@gmail.com>
2024-05-25 16:56:25 +0100

    ADded the instructions in the main file for converter app

--- miniprojects/src/app/converter-app/page.tsx
+++ miniprojects/src/app/converter-app/page.tsx
@@ -1,3 +1,7 @@
+// Create an app to convert units of measurement: 
+// celsius to fahrenheit, kilograms to grams, kilograms to pounds (lbs)
+// kilograms to stones, cm to feet, meters to yards
+
 export default function ConverterApp() {
     return (
         <div></div>
