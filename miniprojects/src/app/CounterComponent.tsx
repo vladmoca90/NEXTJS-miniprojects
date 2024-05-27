794882cebf1418f77781e5d9f696ec5ed29828e7
Vlad Mocanu <vladmoca90@gmail.com>
2024-05-27 22:48:19 +0100

    Created an asynchronous function and the counter works async now

--- miniprojects/src/app/CounterComponent.tsx
+++ miniprojects/src/app/CounterComponent.tsx
@@ -1,16 +1,16 @@
 import { useDispatch, useSelector } from "react-redux";
-import { RootState } from "./counter-redux/state/store";
-import { decrement, increment, incrementByAmount } from "./counter-redux/state/counter/counterState";
+import { AppDispatch, RootState } from "./counter-redux/state/store";
+import { decrement, increment, incrementByAmount, incrementAsync } from "./counter-redux/state/counter/counterState";
 
 export default function CounterComponent() {
     const count = useSelector((state: RootState) => state.counter.value);
-    const dispatch = useDispatch();
+    const dispatch = useDispatch<AppDispatch>(); // needed if you work with asynchronous actions.
 
     return (
         <div>
             <h3>{count}</h3>
             <div className="counter-block">
-                <button onClick={() => dispatch(incrementByAmount(10))}>Increment</button>
+                <button onClick={() => dispatch(incrementAsync(10))}>Increment</button>
                 <button onClick={() => dispatch(decrement())}>Decrement</button>
             </div>
         </div>
--- miniprojects/src/app/counter-redux/state/counter/counterState.ts
+++ miniprojects/src/app/counter-redux/state/counter/counterState.ts
@@ -1,4 +1,4 @@
-import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
 
 interface CounterState {
     value: number;
@@ -24,7 +24,26 @@ const counterSlice = createSlice({
             state.value += action.payload;
         }
     },
+    extraReducers: (builder) => {
+        builder.addCase(
+            incrementAsync.pending, (state) => {
+                console.log("incrementAsync.pending");
+            }
+        ).addCase(
+            incrementAsync.fulfilled, (state, action) => {
+                state.value += action.payload;
+            }
+        );
+    }
 });
+
+export const incrementAsync = createAsyncThunk(
+    "counter/incrementAsync",
+    async (amount: number) => {
+        await new Promise((resolve) => setTimeout(resolve, 1000));
+        return amount;
+    }
+);
 
 export const { increment, decrement, incrementByAmount } = counterSlice.actions; // we will export the actions from the counterSlice({});
 
