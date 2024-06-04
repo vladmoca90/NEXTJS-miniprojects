430e864a211d6caf31c04d2db9ffe4601e0a8002
Vlad Mocanu <vladmoca90@gmail.com>
2024-06-04 23:14:31 +0100

    Started to basically write the functions for the converter

--- miniprojects/src/app/ConverterComponent.tsx
+++ miniprojects/src/app/ConverterComponent.tsx
@@ -1,9 +1,21 @@
+import { useCallback, useState } from "react";
+
 export default function ConverterComponent() {
+    const [unit, setUnit] = useState(0);
+
+    const getCelsiusValue = useCallback(() => {
+
+    }, []);
+
+    const convertCelsiusToFehrenheit = useCallback(() => {
+
+    }, []);
+
     return (
         <div className="converter-container">
             <div>
                 <label className="converter-description">C<sup>o</sup> to F<sup>o</sup></label>
-                <input type="text" className="converter-field" />
+                <input onChange={getCelsiusValue} type="text" className="converter-field" />
                 <span className="converter-result"></span>
                 <button className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-3 border border-gray-400 rounded shadow">
                     Button
