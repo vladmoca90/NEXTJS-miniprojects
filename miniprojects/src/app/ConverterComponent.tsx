94be9f5d9b97a4580753fe00b2d852720b023ebe
Vlad Mocanu <vladmoca90@gmail.com>
2024-06-04 21:21:04 +0100

    Created a ConverterComponent

---
+++ miniprojects/src/app/ConverterComponent.tsx
@@ -0,0 +1,7 @@
+export default function ConverterComponent() {
+    return (
+        <div>
+            
+        </div>
+    );
+}
--- miniprojects/src/app/page.tsx
+++ miniprojects/src/app/page.tsx
@@ -1,9 +1,14 @@
+"use client";
+import ConverterComponent from "./ConverterComponent";
+
 // Create an app to convert units of measurement: 
 // Celsius to fahrenheit, kilograms to grams, kilograms to pounds (lbs)
 // Kilograms to stones, cm to feet, meters to yards
 
 export default function ConverterApp() {
     return (
-        <div></div>
+        <div id="app">
+            <ConverterComponent />
+        </div>
     );
 }
