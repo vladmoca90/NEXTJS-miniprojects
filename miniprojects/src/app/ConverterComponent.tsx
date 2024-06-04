2c4b46cb0381caeb152d8f2e31165f674c85cae1
Vlad Mocanu <vladmoca90@gmail.com>
2024-06-05 00:00:04 +0100

    Started to basically write the functions for the converter #2

--- miniprojects/src/app/ConverterComponent.tsx
+++ miniprojects/src/app/ConverterComponent.tsx
@@ -16,8 +16,8 @@ export default function ConverterComponent() {
             <div>
                 <label className="converter-description">C<sup>o</sup> to F<sup>o</sup></label>
                 <input onChange={getCelsiusValue} type="text" className="converter-field" />
-                <span className="converter-result"></span>
-                <button className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-3 border border-gray-400 rounded shadow">
+                <span className="converter-result">{unit}</span>
+                <button onClick={convertCelsiusToFehrenheit} className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-3 border border-gray-400 rounded shadow">
                     Button
                 </button>
             </div>
