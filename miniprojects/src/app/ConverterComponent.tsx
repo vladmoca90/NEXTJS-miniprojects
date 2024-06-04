3bdfa4b3cf042b4df2636c066a54bf3e8685f2fe
Vlad Mocanu <vladmoca90@gmail.com>
2024-06-04 22:29:25 +0100

    Added a button for the converter

--- miniprojects/src/app/ConverterComponent.tsx
+++ miniprojects/src/app/ConverterComponent.tsx
@@ -5,31 +5,49 @@ export default function ConverterComponent() {
                 <label className="converter-description">C<sup>o</sup> to F<sup>o</sup></label>
                 <input type="text" className="converter-field" />
                 <span className="converter-result"></span>
+                <button className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-3 border border-gray-400 rounded shadow">
+                    Button
+                </button>
             </div>
             <div>
                 <label className="converter-description">kg to g</label>
                 <input type="text" className="converter-field" />
                 <span className="converter-result"></span>
+                <button className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-3 border border-gray-400 rounded shadow">
+                    Button
+                </button>
             </div>
             <div>
                 <label className="converter-description">kg to lbs</label>
                 <input type="text" className="converter-field" />
                 <span className="converter-result"></span>
+                <button className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-3 border border-gray-400 rounded shadow">
+                    Button
+                </button>
             </div>
             <div>
                 <label className="converter-description">kg to st</label>
                 <input type="text" className="converter-field" />
                 <span className="converter-result"></span>
+                <button className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-3 border border-gray-400 rounded shadow">
+                    Button
+                </button>
             </div>
             <div>
                 <label className="converter-description">cm to ft</label>
                 <input type="text" className="converter-field" />
                 <span className="converter-result"></span>
+                <button className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-3 border border-gray-400 rounded shadow">
+                    Button
+                </button>
             </div>
             <div>
                 <label className="converter-description">m to yd</label>
                 <input type="text" className="converter-field" />
                 <span className="converter-result"></span>
+                <button className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-3 border border-gray-400 rounded shadow">
+                    Button
+                </button>
             </div>
         </div>
     );
