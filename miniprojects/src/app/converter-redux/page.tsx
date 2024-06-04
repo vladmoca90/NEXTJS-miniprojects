9a2ce99a7f95ebe279079b67befd85b23688dd36
Vlad Mocanu <vladmoca90@gmail.com>
2024-06-04 19:43:29 +0100

    Will make the converter app with Redux

--- miniprojects/src/app/converter-redux/page.tsx
+++ miniprojects/src/app/converter-redux/page.tsx
@@ -1,6 +1,6 @@
 // Create an app to convert units of measurement: 
-// celsius to fahrenheit, kilograms to grams, kilograms to pounds (lbs)
-// kilograms to stones, cm to feet, meters to yards
+// Celsius to fahrenheit, kilograms to grams, kilograms to pounds (lbs)
+// Kilograms to stones, cm to feet, meters to yards
 
 export default function ConverterApp() {
     return (
