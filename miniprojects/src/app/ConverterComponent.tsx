77500f0f5a12b083dc779e1e256b1a6f32e03978
Vlad Mocanu <vladmoca90@gmail.com>
2024-06-05 20:32:21 +0100

    Removed the click event for now and fixed the Celsius to Fehrenheit display on multiple cases

--- miniprojects/src/app/ConverterComponent.tsx
+++ miniprojects/src/app/ConverterComponent.tsx
@@ -1,7 +1,6 @@
 import { ChangeEvent, useCallback, useState } from "react";
 
 export default function ConverterComponent() {
-    const [unit, setUnit] = useState(0);
     const [unitCelsius, setUnitCelsius] = useState("");
 
     const getCelsiusValue = useCallback(async (e: ChangeEvent<HTMLInputElement>) => {
@@ -10,11 +9,14 @@ export default function ConverterComponent() {
     }, []);
 
     const convertCelsiusToFehrenheit = useCallback(() => {
-        let fehrenheitValue = (parseInt(unitCelsius)*1.8) + 32;
+        let fehrenheitValue = (parseInt(unitCelsius) * 1.8) + 32;
 
-        if(parseInt(unitCelsius) === 0) {
+        if (parseInt(unitCelsius) === 0) {
             return 32;
-        } else {
+        } else if (unitCelsius.length || !unitCelsius) {
+            return 0;
+        }
+        else {
             return fehrenheitValue;
         }
     }, [unitCelsius]);
@@ -25,14 +27,14 @@ export default function ConverterComponent() {
                 <label className="converter-description">C<sup>o</sup> to F<sup>o</sup></label>
                 <input onChange={getCelsiusValue} type="text" className="converter-field" />
                 <span className="converter-result">{convertCelsiusToFehrenheit().toFixed(2)}</span>
-                <button onClick={convertCelsiusToFehrenheit} className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-3 border border-gray-400 rounded shadow">
+                <button className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-3 border border-gray-400 rounded shadow">
                     Button
                 </button>
             </div>
             <div>
                 <label className="converter-description">kg to g</label>
                 <input type="text" className="converter-field" />
-                <span className="converter-result">{unit}</span>
+                <span className="converter-result"></span>
                 <button className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-3 border border-gray-400 rounded shadow">
                     Button
                 </button>
@@ -40,7 +42,7 @@ export default function ConverterComponent() {
             <div>
                 <label className="converter-description">kg to lbs</label>
                 <input type="text" className="converter-field" />
-                <span className="converter-result">{unit}</span>
+                <span className="converter-result"></span>
                 <button className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-3 border border-gray-400 rounded shadow">
                     Button
                 </button>
@@ -48,7 +50,7 @@ export default function ConverterComponent() {
             <div>
                 <label className="converter-description">kg to st</label>
                 <input type="text" className="converter-field" />
-                <span className="converter-result">{unit}</span>
+                <span className="converter-result"></span>
                 <button className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-3 border border-gray-400 rounded shadow">
                     Button
                 </button>
@@ -56,7 +58,7 @@ export default function ConverterComponent() {
             <div>
                 <label className="converter-description">cm to ft</label>
                 <input type="text" className="converter-field" />
-                <span className="converter-result">{unit}</span>
+                <span className="converter-result"></span>
                 <button className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-3 border border-gray-400 rounded shadow">
                     Button
                 </button>
@@ -64,7 +66,7 @@ export default function ConverterComponent() {
             <div>
                 <label className="converter-description">m to yd</label>
                 <input type="text" className="converter-field" />
-                <span className="converter-result">{unit}</span>
+                <span className="converter-result"></span>
                 <button className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-3 border border-gray-400 rounded shadow">
                     Button
                 </button>
