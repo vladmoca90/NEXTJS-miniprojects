3869536b714075a2f59b1b2223e2f480e8f4f7ad
Vlad Mocanu <vladmoca90@gmail.com>
2024-08-12 20:37:34 +0100

    Added the correct HTML and solved all the bugs in the console

--- miniprojects/src/app/countriesListComponent.tsx
+++ miniprojects/src/app/countriesListComponent.tsx
@@ -8,17 +8,21 @@ export function CountriesListComponent(props: countriesListProps) {
     const { country } = props;
 
     return (
-        <ul className="countries-list">
-            <li className="list-title">
-                <span className="country-name">Name</span>
-                <span className="country-name">Official name</span>
-                <span className="country-img">Flag</span>
-                <span className="country-description">Flag description</span>
-            </li>
-            <li key={props.country.index}>{props.country.name.common}</li>
-            <li key={props.country.index}>{props.country.name.official}</li>
-            <li key={props.country.index}>{props.country.flags.png}</li>
-            <li key={props.country.index}>{props.country.flags.alt}</li>
-        </ul>
+        <>
+            <ul className="countries-list">
+                <li className="list-title">
+                    <span className="country-name">Name</span>
+                    <span className="country-name">Official name</span>
+                    <span className="country-img">Flag</span>
+                    <span className="country-description">Flag description</span>
+                </li>
+                <li key={country.index} data-country={country.name.common}>
+                    <span key={country.index}>{country.name.common}</span>
+                    <span key={country.index}>{country.name.official}</span>
+                    <span key={country.index}>{country.flags.png}</span>
+                    <span key={country.index}>{country.flags.alt}</span>
+                </li>
+            </ul>
+        </>
     )
 }
