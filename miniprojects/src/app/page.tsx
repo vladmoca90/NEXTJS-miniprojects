b177608b9a2f915903b5d8a993bf143a285ee55d
Vlad Mocanu <vladmoca90@gmail.com>
2024-08-14 22:32:50 +0100

    Fixed the component bugs

--- miniprojects/src/app/page.tsx
+++ miniprojects/src/app/page.tsx
@@ -1,5 +1,5 @@
-import { NavbarComponent } from "./counter-redux2/NavbarComponent";
-import { StoreComponent } from "./counter-redux2/store/StoreComponent";
+import NavbarComponent from "./counter-redux2/NavbarComponent";
+import StoreComponent from "./counter-redux2/store/StoreComponent";
 
 export default function Counter() {
     return (
