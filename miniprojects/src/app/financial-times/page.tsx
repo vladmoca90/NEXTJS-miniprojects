db3311f2bd2c9a933a781a2137b9ed79e1a99c5b
vladmoca90 <vladmoca90@gmail.com>
2024-10-09 22:29:31 +0100

    Deleted the Financial Times component

--- miniprojects/src/app/financial-times/page.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-import "./../styles/financial-times.css";
-import { FinancialTimesComponent } from "./FinancialTimesComponent";
-
-export default function FinancialTimesMain() {
-    const quotesUrl = "https://markets-data-api-proxy.ft.com/research/webservices/securities/v1/quotes?symbols=FTSE:FSI,INX:IOM,EURUSD,GBPUSD,IB.1:IEU";
-
-    <FinancialTimesComponent />
-}
