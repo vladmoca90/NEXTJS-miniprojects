d3cbef57036ec75c101416d8d08bcb3f18442eae
vladmoca90 <vladmoca90@gmail.com>
2025-02-18 23:25:20 +0000

    Deleted comments from the Transactions project

--- miniprojects/src/app/transactions-context/transactionContext/TransactionContext.tsx
+++ miniprojects/src/app/transactions-context/transactionContext/TransactionContext.tsx
@@ -1,20 +1,10 @@
-// import { createContext } from "react";
-// import { Transaction } from "../../../../data/transactions/Transaction";
-
-// export const TransactionContext = createContext<Transaction[]>([]);
-
-
-// NEW VERSION //
-
 import { createContext } from "react";
 import { Transaction } from "../../../../data/transactions/Transaction";
 
-// Define the shape of the context value
 interface TransactionContextType {
     transactions: Transaction[];
 
-    setTransactions: (transactions: Transaction[]) => void; // You can add more functions or properties as needed
+    setTransactions: (transactions: Transaction[]) => void;
 }
 
-// Create a context for managing transactions with default values
 export const TransactionContext = createContext<TransactionContextType | undefined>(undefined);
