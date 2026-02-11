3e9f70a7a2e717659fd90501701951c4a32c0237
Vlad Mocanu <vladmoca90@gmail.com>
2026-02-11 23:36:16 +0000

    Amended the project so it opens automatically in Chrome

--- miniprojects/next.config.js
+++ miniprojects/next.config.js
@@ -2,3 +2,8 @@
 const nextConfig = {}
 
 module.exports = nextConfig
+
+// Open Chrome automatically in development
+if (process.env.NODE_ENV === 'development') {
+  require('child_process').exec('start chrome http://localhost:3000')
+}
