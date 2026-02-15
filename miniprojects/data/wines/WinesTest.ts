9b7827f38e4a9c83ddaf607cfac4ebadb8f78a0a
Vlad Mocanu <vladmoca90@gmail.com>
2026-02-15 22:11:57 +0000

    Added the Wines Test data in a separate file and folder

---
+++ miniprojects/data/wines/WinesTest.ts
@@ -0,0 +1,28 @@
+import { Wine } from './Wine';
+
+export const mockWines: Wine[] = [
+  {
+    index: 1,
+    name: 'Cabernet Sauvignon',
+    img: 'cabernet.jpg',
+    text: 'A full-bodied red wine'
+  },
+  {
+    index: 2,
+    name: 'Chardonnay',
+    img: 'chardonnay.jpg',
+    text: 'A crisp white wine'
+  },
+  {
+    index: 3,
+    name: 'Merlot',
+    img: 'merlot.jpg',
+    text: 'A smooth red wine'
+  },
+  {
+    index: 4,
+    name: 'Sauvignon Blanc',
+    img: 'sauvignon.jpg',
+    text: 'A fresh white wine'
+  }
+];
--- miniprojects/tests/wineFilterCheckboxes.test.ts
+++ miniprojects/tests/wineFilterCheckboxes.test.ts
@@ -1,32 +1,4 @@
-import { Wine } from '../data/wines/Wine';
-
-// Mock Wine data
-const mockWines: Wine[] = [
-  {
-    index: 1,
-    name: 'Cabernet Sauvignon',
-    img: 'cabernet.jpg',
-    text: 'A full-bodied red wine'
-  },
-  {
-    index: 2,
-    name: 'Chardonnay',
-    img: 'chardonnay.jpg',
-    text: 'A crisp white wine'
-  },
-  {
-    index: 3,
-    name: 'Merlot',
-    img: 'merlot.jpg',
-    text: 'A smooth red wine'
-  },
-  {
-    index: 4,
-    name: 'Sauvignon Blanc',
-    img: 'sauvignon.jpg',
-    text: 'A fresh white wine'
-  }
-];
+import { mockWines } from '../data/wines/WinesTest';
 
 describe('Wine Filter Functionality', () => {
   
