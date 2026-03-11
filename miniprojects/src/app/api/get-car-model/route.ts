e31da3ed06e747dbc2e7b4bacdceaa8617e36023
Vlad Mocanu <vladmoca90@gmail.com>
2026-03-11 20:20:24 +0000

    Trying to fix the bug for the get car model

--- miniprojects/src/app/api/get-car-model/route.ts
+++ miniprojects/src/app/api/get-car-model/route.ts
@@ -3,36 +3,21 @@ import { allCars } from "../../../../data/cars/allCars";
 
 export async function GET(request: NextRequest) {
     const carModel = request.nextUrl.searchParams.get("carModel");
+    console.log("Received carModel:", carModel);
 
     if (carModel === null) {
-        return NextResponse.json(
-            {},
-            {
-                status: 400
-            }
-        );
+        console.log("No carModel provided");
+        return NextResponse.json({}, { status: 400 });
     }
 
-    const car = allCars.find((car) => {
-        return carModel === car.model;
-    });
+    const car = allCars.find((car) => carModel === car.model);
+    console.log("Found car:", car);
 
     if (!car) {
-        return NextResponse.json(
-            {},
-            {
-                status: 404
-            }
-        );
+        console.log("Car not found for model:", carModel);
+        return NextResponse.json({}, { status: 404 });
     } else {
-        return NextResponse.json(
-            {
-                car
-            },
-            {
-                status: 200
-            },
-        );
+        return NextResponse.json({ car }, { status: 200 });
     }
 }
 
