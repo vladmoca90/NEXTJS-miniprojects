4356d06fc3b78f10ee4048e79119414fbea70cd6
Vlad Mocanu <vladmoca90@gmail.com>
2026-03-19 20:04:59 +0000

    Fixed the ToDoList page.tsx issue

--- miniprojects/src/app/page.tsx
+++ miniprojects/src/app/page.tsx
@@ -1,58 +1,17 @@
 "use client";
+import "./styles/to-do-list.css";
+import React from "react";
+import { Provider } from 'react-redux';
+import { store } from './to-do-list/store/store';
+import ToDoListComponent from "./to-do-list/ToDoListComponent";
 
-import "./styles/send-message.css";
-import { ChangeEvent, FormEvent, useCallback, useState } from "react";
-import { useRouter } from "next/navigation";
-
-export default function SendMessage() {
-    const [message, setMessage] = useState<string>("");
-    const router = useRouter();
-
-    const handleChange = useCallback(
-        (e: ChangeEvent<HTMLTextAreaElement>) => {
-            setMessage(e.target.value);
-        },
-        []
-    );
-
-    const handleSubmit = useCallback(
-        (e: FormEvent<HTMLFormElement>) => {
-            e.preventDefault();
-
-            if (!message.trim()) return;
-
-            router.push(
-                `/send-message/message-value?messageText=${encodeURIComponent(message)}`
-            );
-        },
-        [message, router]
-    );
-
+export default function Home() {
     return (
-        <div id="sendText">
-            <form className="max-w-sm mx-auto" onSubmit={handleSubmit}>
-                <label
-                    htmlFor="message"
-                    className="block mb-2 text-sm font-medium text-gray-900"
-                >
-                    Your message
-                </label>
-
-                <textarea
-                    id="message"
-                    name="message"
-                    value={message}
-                    onChange={handleChange}
-                    placeholder="Leave a comment..."
-                    className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500"
-                />
-
-                <button type="submit" className="messageBtn">
-                    Submit
-                </button>
-            </form>
-
-            <p id="yourText">{message}</p>
+        <Provider store={store}>
+        <div>
+            <h1>My ToDo List</h1>
+            <ToDoListComponent />
         </div>
+        </Provider>
     );
 }
---
+++ miniprojects/src/app/send-message/page.tsx
@@ -0,0 +1,57 @@
+"use client";
+import "./styles/send-message.css";
+import { ChangeEvent, FormEvent, useCallback, useState } from "react";
+import { useRouter } from "next/navigation";
+
+export default function SendMessage() {
+    const [message, setMessage] = useState<string>("");
+    const router = useRouter();
+
+    const handleChange = useCallback(
+        (e: ChangeEvent<HTMLTextAreaElement>) => {
+            setMessage(e.target.value);
+        },
+        []
+    );
+
+    const handleSubmit = useCallback(
+        (e: FormEvent<HTMLFormElement>) => {
+            e.preventDefault();
+
+            if (!message.trim()) return;
+
+            router.push(
+                `/send-message/message-value?messageText=${encodeURIComponent(message)}`
+            );
+        },
+        [message, router]
+    );
+
+    return (
+        <div id="sendText">
+            <form className="max-w-sm mx-auto" onSubmit={handleSubmit}>
+                <label
+                    htmlFor="message"
+                    className="block mb-2 text-sm font-medium text-gray-900"
+                >
+                    Your message
+                </label>
+
+                <textarea
+                    id="message"
+                    name="message"
+                    value={message}
+                    onChange={handleChange}
+                    placeholder="Leave a comment..."
+                    className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500"
+                />
+
+                <button type="submit" className="messageBtn">
+                    Submit
+                </button>
+            </form>
+
+            <p id="yourText">{message}</p>
+        </div>
+    );
+}
