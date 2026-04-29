9e6c22313e5e161fb57bd021e8437ded4c25fff6
Vlad Mocanu <vladmoca90@gmail.com>
2026-04-29 23:53:39 +0100

    Added the image paths for the actors project

--- miniprojects/data/actors/AllActors.ts
+++ miniprojects/data/actors/AllActors.ts
@@ -3,27 +3,27 @@ import { Actor } from "./Actor";
 
 export const AllActors: Actor[] = [
     {
-        img: "",
+        img: "/images/actors/russell-crowe.jpg",
         name: "Russell Crowe",
         biography: "Russell Ira Crowe is an actor and film director. His work on screen has earned him various accolades, including an Academy Award, two Golden Globe Awards, and a British Academy Film Award. Crowe was born in New Zealand, moving to Australia at the age of four and residing there permanently by the age of 21.",
     },
     {
-        img: "",
+        img: "/images/actors/natalie-portman.jpg",
         name: "Natalie Portman",
         biography: "Natalie Portman, is an actress, film producer and director with dual Israeli and American citizenship. She has had a prolific screen career from her teenage years and has starred in various blockbusters and independent films, receiving multiple accolades, including an Academy Award, a BAFTA Award, and two Golden Globe Awards.",
     },
     {
-        img: "",
+        img: "/images/actors/hugh-jackman.jpg",
         name: "Hugh Jackman",
         biography: "Hugh Michael Jackman is an Australian actor, singer, and producer. Starting his career in theatre and television, Jackman landed his breakthrough role as Wolverine, playing him across the X-Men film franchise and in the Marvel Cinematic Universe from X-Men to Deadpool & Wolverine.",
     },
     {
-        img: "",
+        img: "/images/actors/luke-evans.jpg",
         name: "Luke Evans",
         biography: "Russell Ira Crowe is an actor and film director. His work on screen has earned him various accolades, including an Academy Award, two Golden Globe Awards, and a British Academy Film Award. Crowe was born in New Zealand, moving to Australia at the age of four and residing there permanently by the age of 21.",
     },
     {
-        img: "",
+        img: "/images/actors/orlando-bloom.avif",
         name: "Orlando Bloom",
         biography: "Orlando Jonathan Blanchard Copeland Bloom is an English actor. He made his breakthrough as the character Legolas in The Lord of the Rings film series, earning him three Actors Award nominations, winning once. He reprised his role in The Hobbit film series.",
     },
