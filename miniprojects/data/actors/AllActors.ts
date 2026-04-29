7dbb1b120b163861558312115b6a368c6a8218e3
Vlad Mocanu <vladmoca90@gmail.com>
2026-04-29 22:23:59 +0100

    Added 3 images of actors and some data in the json

--- miniprojects/data/actors/AllActors.ts
+++ miniprojects/data/actors/AllActors.ts
@@ -4,18 +4,18 @@ import { Actor } from "./Actor";
 export const AllActors: Actor[] = [
     {
         img: "",
-        name: "",
-        biography: "",
+        name: "Russell Crowe",
+        biography: "Russell Ira Crowe is an actor and film director. His work on screen has earned him various accolades, including an Academy Award, two Golden Globe Awards, and a British Academy Film Award. Crowe was born in New Zealand, moving to Australia at the age of four and residing there permanently by the age of 21.",
     },
     {
         img: "",
-        name: "",
-        biography: "",
+        name: "Natalie Portman",
+        biography: "Natalie Portman, is an actress, film producer and director with dual Israeli and American citizenship. She has had a prolific screen career from her teenage years and has starred in various blockbusters and independent films, receiving multiple accolades, including an Academy Award, a BAFTA Award, and two Golden Globe Awards.",
     },
     {
         img: "",
-        name: "",
-        biography: "",
+        name: "Hugh Jackman",
+        biography: "Hugh Michael Jackman is an Australian actor, singer, and producer. Starting his career in theatre and television, Jackman landed his breakthrough role as Wolverine, playing him across the X-Men film franchise and in the Marvel Cinematic Universe from X-Men to Deadpool & Wolverine.",
     },
     {
         img: "",
