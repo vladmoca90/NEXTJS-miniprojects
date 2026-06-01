38b6489d66eca5c98abaa811552af1e9ec35c163
Vlad Mocanu <vladmoca90@gmail.com>
2026-06-01 01:04:04 +0100

    Started to rearrange everything for the math game

--- miniprojects/src/app/api/math-game/route.ts
+++ miniprojects/src/app/api/math-game/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse, NextRequest } from "next/server";
-import { mathQuestions } from "../../../../data/math/questions";
+import { mathQuestions } from "../../../../data/math-game/mathQuestions";
 
 export async function GET(request: NextRequest) {
   return NextResponse.json(
