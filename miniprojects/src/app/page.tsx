ecb901db3ebf9c0afeac2bdae5003858137d4c9f
Vlad Mocanu <vladmoca90@gmail.com>
2026-06-07 11:56:30 +0100

    The bug is fixed and project works now

--- miniprojects/src/app/page.tsx
+++ miniprojects/src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React, { useRef, useState, useEffect } from 'react'
-import styles from '../styles/moving-square.module.css'
+import styles from './styles/moving-square.module.css'
 
 const squareSize = 60
 
