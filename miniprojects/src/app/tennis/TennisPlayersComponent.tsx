e30aef0c00179b279a6d36356ba9ff1f266e12ac
Vlad Mocanu <vladmoca90@gmail.com>
2026-07-21 14:55:38 +0100

    Started writing the code for the Tennis component

---
+++ miniprojects/src/app/tennis/TennisPlayersComponent.tsx
@@ -0,0 +1,12 @@
+export const TennisPlayersComponent = () => {
+    return (
+        <div>
+            <h2>Tennis Players</h2>
+            <ul>
+                <li>Rafael Nadal</li>
+                <li>Novak Djokovic</li>
+                <li>Stanislas Wawrinka</li>
+            </ul>
+        </div>
+    );
+}
---
+++ miniprojects/src/app/tennis/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+import { TennisPlayersComponent } from './TennisPlayersComponent';
+
+export default function TennisPage() {
+    return (
+        <div>
+            <TennisPlayersComponent />
+        </div>
+    );
+}
