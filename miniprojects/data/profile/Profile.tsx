df6fa271efdb8fa77c36c26fa80f7a0e2c58e320
Vlad Mocanu <vladmoca90@gmail.com>
2026-07-25 22:20:53 +0100

    Created a ProfilePerson.tsx file for the perosn profile

--- miniprojects/data/profile/Profile.tsx
+++ miniprojects/data/profile/Profile.tsx
@@ -5,11 +5,3 @@ export interface ProfileData { // Renamed to ProfileData
     occupation: string;
     surname: string;
 }
-
-export const Profile: ProfileData = { // Updated to use the new interface name
-    age: 33,
-    forename: "Jack",
-    nationality: "British",
-    occupation: "Senior Design Producer",
-    surname: "McGovern",
-};
---
+++ miniprojects/data/profile/ProfilePerson.tsx
@@ -0,0 +1,9 @@
+import { ProfileData } from "./ProfileData"; // Import the interface
+
+export const Profile: ProfileData = { // Updated to use the new interface name
+    age: 33,
+    forename: "Jack",
+    nationality: "British",
+    occupation: "Senior Design Producer",
+    surname: "McGovern",
+};
