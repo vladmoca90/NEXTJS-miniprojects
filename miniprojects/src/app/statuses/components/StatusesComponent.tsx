f17b2c77bde96ea03af853f2601f57f2739900ec
Vlad Mocanu <vladmoca90@gmail.com>
2026-08-25 01:54:58 +0100

    Started to create the components for Statuses project

---
+++ miniprojects/src/app/statuses/components/StatusesComponent.tsx
@@ -0,0 +1,7 @@
+export const StatusesComponent = () => {
+    return (
+        <div className="statuses-container">
+            <button className="status-button">Status 1</button>
+        </div>
+    );
+}
---
+++ miniprojects/src/app/statuses/page.tsx
@@ -0,0 +1,9 @@
+import { StatusesComponent } from "./components/StatusesComponent";
+
+export default function StatusesPage() {
+  return (
+    <div>
+      <StatusesComponent />
+    </div>
+  );
+}
