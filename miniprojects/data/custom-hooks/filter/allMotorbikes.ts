b0aa1f6896d2d8bc29477fe24e4da23235490ca1
Vlad Mocanu <vladmoca90@gmail.com>
2026-09-10 15:55:12 +0100

    Added a few more motorbike brands

--- miniprojects/data/custom-hooks/filter/allMotorbikes.ts
+++ miniprojects/data/custom-hooks/filter/allMotorbikes.ts
@@ -1,8 +1,28 @@
 import { Motorbike } from "./Motorbike";
 
 export const names: Motorbike[] = [
-  { name: "Honda" },
-  { name: "Yamaha" },
-  { name: "Ducati" },
-  { name: "Kawasaki" },
+  { 
+    name: "Honda" 
+  },
+  { 
+    name: "Yamaha" 
+  },
+  { 
+    name: "Ducati" 
+  },
+  { 
+    name: "Kawasaki" 
+  },
+  { 
+    name: "Suzuki" 
+  },
+  { 
+    name: "Harley-Davidson" 
+  },
+   { 
+    name: "BMW Motorrad" 
+  },
+  { 
+    name: "KTM" 
+  },
 ];
